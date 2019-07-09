c63b1f53945b1ca5f09c8a9fd398ff3a607c6f65
Mohamed Labib <lebaz20@gmail.com>
2019-07-09 15:00:29 +0200

    #40 Add pagination tests

--- src/components/Atoms/Pagination/Pagination.test.js
+++ src/components/Atoms/Pagination/Pagination.test.js
@@ -1,117 +1,99 @@
 import React from 'react';
 import 'jest-styled-components';
 import renderWithTheme from '../../../hoc/shallowWithTheme';
-import TextArea from './TextArea';
+import Pagination from './Pagination';
 
-it('renders correctly', () => {
+it('renders correctly in minimalist form', () => {
   const tree = renderWithTheme(
-    <TextArea
-      name="description"
-      placeholder="This is text area"
-      label="Label"
-      rows="5"
-      cols="5"
-      errorMsg=""
-      id="Please leave your comment here"
+    <Pagination
+      maxPages={5}
+      totalPages={5}
+      currentPage={3}
+      createURL={value => `example.com/page/${value}`}
+      onSelect={value => value}
+      showFirst={false}
+      showPrevious={false}
+      showNext={false}
+      showLast={false}
     />
   ).toJSON();
 
-  expect(tree).toMatchInlineSnapshot(`
-    .c1 {
-      text-transform: inherit;
-      font-weight: bold;
-    }
+  expect(tree).toMatchSnapshot();
+});
 
-    .c2 {
-      box-sizing: border-box;
-      width: 100%;
-      margin: 10px 0;
-      min-height: 65px;
-      padding: 6px 12px;
-      font-size: 1.2rem;
-      background-color: #FFFFFF;
-      border: 1px solid;
-      border-radius: 0;
-      border-color: #030e1a;
-      box-shadow: none;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      appearance: none;
-      color: #030e1a;
-    }
+it('renders correctly in full size with max pages equal to total pages', () => {
+  const tree = renderWithTheme(
+    <Pagination
+      maxPages={5}
+      totalPages={5}
+      currentPage={3}
+      createURL={value => `example.com/page/${value}`}
+      onSelect={value => value}
+      showFirst
+      showPrevious
+      showNext
+      showLast
+    />
+  ).toJSON();
 
-    .c2:focus {
-      border: 1px solid #666;
-    }
+  expect(tree).toMatchSnapshot();
+});
 
-    .c2:focus::-webkit-input-placeholder {
-      color: transparent;
-      color: $color;
-      font-size: $font-size;
-      opacity: 1;
-      overflow: visible;
-    }
+it('renders correctly in full size with max pages more than total pages', () => {
+  const tree = renderWithTheme(
+    <Pagination
+      maxPages={10}
+      totalPages={6}
+      currentPage={4}
+      createURL={value => `example.com/page/${value}`}
+      onSelect={value => value}
+      showFirst
+      showPrevious
+      showNext
+      showLast
+    />
+  ).toJSON();
 
-    .c2:focus:-moz-placeholder {
-      color: transparent;
-      color: $color;
-      font-size: $font-size;
-      opacity: 1;
-      overflow: visible;
-    }
+  expect(tree).toMatchSnapshot();
+});
 
-    .c2:focus::-moz-placeholder {
-      color: transparent;
-      color: $color;
-      font-size: $font-size;
-      opacity: 1;
-      overflow: visible;
-    }
+it('renders correctly in full size with max pages less than total pages', () => {
+  const tree = renderWithTheme(
+    <Pagination
+      maxPages={6}
+      totalPages={10}
+      currentPage={2}
+      createURL={value => `example.com/page/${value}`}
+      onSelect={value => value}
+      showFirst
+      showPrevious
+      showNext
+      showLast
+    />
+  ).toJSON();
 
-    .c2:focus:-ms-input-placeholder {
-      color: transparent;
-      color: $color;
-      font-size: $font-size;
-      opacity: 1;
-      overflow: visible;
-    }
+  expect(tree).toMatchSnapshot();
+});
 
-    .c0 {
-      display: -webkit-box;
-      display: -webkit-flex;
-      display: -ms-flexbox;
-      display: flex;
-      -webkit-flex-direction: column;
-      -ms-flex-direction: column;
-      flex-direction: column;
-    }
+it('renders correctly with custom labels', () => {
+  const tree = renderWithTheme(
+    <Pagination
+      maxPages={5}
+      totalPages={5}
+      currentPage={3}
+      createURL={value => `example.com/page/${value}`}
+      onSelect={value => value}
+      showFirst
+      showPrevious
+      showNext
+      showLast
+      previousLabel="Previous"
+      nextLabel="Next"
+      firstLabel="First"
+      lastLabel="Last"
+      getPageLabel={value => `Page ${value}`}
+    />
+  ).toJSON();
 
-    @media (min-width:740px) {
-      .c2 {
-        max-width: 100%;
-        height: 96px;
-      }
-    }
-
-    <label
-      className="c0"
-      htmlFor="undefined"
-    >
-      <span
-        className="c1"
-        color="inherit"
-      >
-        Label
-      </span>
-      <textarea
-        aria-describedby="Please leave your comment here"
-        className="c2"
-        cols="5"
-        name="description"
-        placeholder="This is text area"
-        rows="5"
-      />
-      
-    </label>
-  `);
+  expect(tree).toMatchSnapshot();
 });
