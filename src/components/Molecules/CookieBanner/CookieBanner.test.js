1509f4d980f7b0dd603b85f95d5af84aa624d200
louiaR <louitiani@gmail.com>
2019-08-02 11:25:52 +0100

    Testing

--- src/components/Molecules/CookieBanner/CookieBanner.test.js
+++ src/components/Molecules/CookieBanner/CookieBanner.test.js
@@ -18,5 +18,136 @@ it('renders correctly', () => {
     />
   ).toJSON();
 
-  expect(tree).toMatchInlineSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    .c2 {
+      color: #FFFFFF;
+      text-transform: inherit;
+    }
+
+    .c4 {
+      color: #000;
+      -webkit-text-decoration: none;
+      text-decoration: none;
+      display: inline-block;
+      padding: 0 2px 1px;
+      border-bottom: 2px solid;
+      border-bottom-color: #000;
+    }
+
+    .c6 {
+      display: inline-block;
+      padding: 11px 17px;
+      text-align: center;
+      -webkit-text-decoration: none;
+      text-decoration: none;
+      font-weight: 700;
+      border-radius: 30px;
+      cursor: point;
+      background-color: #FFFFFF;
+    }
+
+    .c0 {
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      position: relative;
+      padding: 20px;
+      background: #030e1a;
+      -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      z-index: 3;
+    }
+
+    .c1 {
+      margin: 0;
+    }
+
+    .c1:first-of-type {
+      margin-bottom: 10px;
+    }
+
+    .c5 {
+      color: #030e1a;
+      padding: 8px 20px;
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    .c3 {
+      color: #FFFFFF;
+      padding: 0 3px 1px;
+      border-bottom: 2px solid;
+    }
+
+    @media (min-width:1440px) {
+      .c6 {
+        padding: 16px 30px;
+      }
+    }
+
+    @media (min-width:740px) {
+      .c1 {
+        width: 75%;
+        margin: 0 auto;
+      }
+
+      .c1:first-of-type {
+        margin-bottom: 15px;
+      }
+    }
+
+    @media (min-width:740px) {
+      .c5 {
+        margin: 0 6px 0 0;
+        display: inline-block;
+      }
+    }
+
+    <div
+      className="c0"
+    >
+      <p
+        className="c1 c2"
+        color="white"
+      >
+        Hello! Comic Relief uses cookies to help make this website better and improve our services. You can learn more about our use of cookies
+        <a
+          className="c3 c4"
+          color="red"
+          href="https://www.comicrelief.com/cookies-policy"
+          target="_self"
+        >
+          here
+        </a>
+        . We also use optional cookies for marketing purposes:
+      </p>
+      <p
+        className="c1 c2"
+        color="white"
+      >
+        <a
+          className="c5 c6"
+          color="white"
+          href="/comic"
+          onClick={[Function]}
+          target="_self"
+        >
+          Accept
+        </a>
+        Or click
+        <a
+          className="c3 c4"
+          color="red"
+          href=""
+          onClick={[Function]}
+          target="_self"
+        >
+          here
+        </a>
+        to deny these.
+      </p>
+    </div>
+  `);
 });
