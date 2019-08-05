d6718eb08e478efc625b2c7b02e487a7e2dd12e0
Andy E Phipps <a.phipps@comicrelief.com>
2019-08-05 11:23:41 +0100

    Update test content

--- src/components/Molecules/Footer/data/data.js
+++ src/components/Molecules/Footer/data/data.js
@@ -3,110 +3,148 @@ export default {
   menuGroup: [
     {
       id: 'f7dc5eef-f4eb-5405-8a35-08808b55cb33',
-      title: 'Fundraising',
-      url: 'https://www.comicrelief.com/fundraising/pay-in-your-money',
+      title: 'Hear from us',
+      url: 'https://www.comicrelief.com',
       links: [
         {
-          title: 'Red Nose Day',
-          url: 'https://www.comicrelief.com/rednoseday',
+          title: 'Get the newsletter',
+          url: 'https://www.comicrelief.com/#',
           class: null
         },
         {
-          title: 'Regular donations',
-          url: 'https://www.comicrelief.com/join',
-          class: null
-        },
-        {
-          title: 'Squads',
-          url: 'https://www.comicrelief.com/squads',
-          class: null
-        },
-        {
-          title: 'The Noseys',
-          url: 'https://www.comicrelief.com/rednoseday/fundraising/the-noseys',
-          class: null
-        },
-        {
-          title: 'Free downloads',
-          url:
-            'https://www.comicrelief.com/rednoseday/fundraising/free-downloads',
+          title: 'How we contact you',
+          url: 'https://www.comicrelief.com/update-your-preferences',
           class: null
         }
       ]
     },
     {
       id: '9f3980f3-d02b-52e9-ac41-933a778c040a',
-      title: 'What your money does',
-      url: 'https://www.comicrelief.com/what-your-money-does',
+      title: 'Get in touch',
+      url: 'https://www.comicrelief.com/',
       links: [
         {
-          title: 'What your money does',
-          url: 'https://www.comicrelief.com/What-we-do/our-legacy',
+          title: 'Contact us',
+          url: 'https://www.comicrelief.com/contact-us',
+          class: null
+        },
+        {
+          title: 'Your Gift Aid',
+          url: 'https://giftaid.comicrelief.com/update',
+          class: null
+        },
+        {
+          title: 'FAQs',
+          url: 'https://www.comicrelief.com/frequently-asked-questions',
+          class: null
+        },
+        {
+          title: 'Red Nose Day 2019 FAQs',
+          url:
+            'https://www.comicrelief.com/red-nose-days-frequently-asked-questions',
           class: null
         }
       ]
     },
     {
       id: 'eaec5191-bbb3-5e8d-b966-c53fff34998b',
-      title: 'Schools & youth',
-      url: 'https://www.comicrelief.com/rednoseday/schools',
+      title: 'About us',
+      url: 'https://www.comicrelief.com/',
       links: [
         {
-          title: 'Primary schools',
-          url: 'https://www.comicrelief.com/rednoseday/schools/primary-schools',
+          title: 'Mission',
+          url: 'https://www.comicrelief.com/about-comic-relief',
           class: null
         },
         {
-          title: 'Secondary schools',
-          url:
-            'https://www.comicrelief.com/rednoseday/schools/secondary-schools',
+          title: 'Our history',
+          url: 'https://www.comicrelief.com/about-comic-relief/history',
           class: null
         },
         {
-          title: 'Nurseries',
-          url: 'https://www.comicrelief.com/rednoseday/schools/nurseries',
+          title: 'Finances',
+          url: 'https://www.comicrelief.com/about-comic-relief/finances',
           class: null
         },
         {
-          title: 'Youth groups',
-          url: 'https://www.comicrelief.com/rednoseday/youth',
+          title: 'Partners',
+          url: 'https://www.comicrelief.com/working-with-us',
           class: null
         },
         {
-          title: 'Free downloads',
-          url: 'https://www.comicrelief.com/rednoseday/schools/free-downloads',
+          title: 'Meet the team',
+          url: 'https://www.comicrelief.com/about-comic-relief/meet-the-team',
+          class: null
+        },
+        {
+          title: 'Sport Relief',
+          url: 'https://www.sportrelief.com/',
           class: null
         }
       ]
     },
     {
       id: '4a5b055f-1f7e-5f4b-827d-768032fd373c',
-      title: 'Funding',
-      url: 'https://www.comicrelief.com/funding',
+      title: 'Careers',
+      url: 'https://www.comicrelief.com',
       links: [
         {
-          title: 'Funding opportunities',
-          url: 'https://www.comicrelief.com/funding/funding-opportunities',
+          title: 'Working at Comic Relief',
+          url: 'https://www.comicrelief.com/careers',
           class: null
         },
         {
-          title: 'Our strategy',
-          url: 'https://www.comicrelief.com/funding/funding-strategy',
+          title: 'Open rolls',
+          url: 'https://app.beapplied.com/org/comic-relief',
+          class: null
+        }
+      ]
+    },
+    {
+      id: '4a5b055f-1f7e-5f4b-827d-768032fd373d',
+      title: 'News',
+      url: 'https://www.comicrelief.com',
+      links: [
+        {
+          title: 'News',
+          url: 'https://www.comicrelief.com/news',
           class: null
         },
         {
-          title: 'Eligibility',
-          url: 'https://www.comicrelief.com/funding/eligibility-criteria',
+          title: 'Open rolls',
+          url: 'https://www.comicrelief.com/press-releases',
+          class: null
+        }
+      ]
+    },
+    {
+      id: 'eaec5191-bbb3-5e8d-b966-c53fff34998a',
+      title: 'Legal',
+      url: 'https://www.comicrelief.com/',
+      links: [
+        {
+          title: 'Privacy policy',
+          url: 'https://www.comicrelief.com/privacy-notice',
           class: null
         },
         {
-          title: 'Applying for funding',
-          url: 'https://www.comicrelief.com/funding/applying-for-funding',
+          title: 'Terms of use',
+          url: 'https://www.comicrelief.com/terms-of-use',
           class: null
         },
         {
-          title: 'Managing your funding',
-          url: 'https://www.comicrelief.com/funding/managing-your-funding',
+          title: 'Cookies',
+          url: 'https://www.comicrelief.com/cookies-policy',
+          class: null
+        },
+        {
+          title: 'Positive practices',
+          url: 'https://www.comicrelief.com/positive-practices',
+          class: null
+        },
+        {
+          title: 'Text to donate terms of use',
+          url: 'https://www.comicrelief.com/legal/sms',
           class: null
         }
       ]
