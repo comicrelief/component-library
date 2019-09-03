096a95661ea3b637c14e6f31e8b0e9b1b058655f
Heleen3258 <h.mol@comicrelief.com>
2019-09-03 11:05:35 +0200

    133: updated text test

--- src/components/Atoms/Text/Text.test.js
+++ src/components/Atoms/Text/Text.test.js
@@ -5,24 +5,28 @@ import Text from './Text';
 
 it('renders correctly', () => {
   const tree = renderWithTheme(
-    <Text tag="p" size="xl" color="yellow">
-      My paragraph small and yellow
+    <Text tag="p" size="xl" color="yellow" family="Anton">
+      My paragraph extra large, yellow and font family Anton
     </Text>
   ).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
     .c0 {
       color: #FFE400;
+      font-weight: normal;
+      font-family: 'Anton',Impact,sans-serif;
       font-size: 1.725rem;
       text-transform: inherit;
+      
     }
 
     <p
       className="c0"
       color="yellow"
+      family="Anton"
       size="xl"
     >
-      My paragraph small and yellow
+      My paragraph extra large, yellow and font family Anton
     </p>
   `);
 });
