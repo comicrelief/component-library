46d01f99866d7a9735aa22e25a2515ed97552dc3
Gustavo Liedke <g.liedke@comicrelief.com>
2019-09-27 11:01:54 +0100

    fix img height

--- src/components/Molecules/SingleMessage/SingleMessage.style.js
+++ src/components/Molecules/SingleMessage/SingleMessage.style.js
@@ -126,14 +126,11 @@ const PlayButton = styled.button`
 
 const Image = styled.div`
   width: 100%;
-  ${({ vhFull }) => vhFull && 'height: 100%'};
+  height: 100%;
   ${({ doubleImage }) => doubleImage && 'height: 50%'};
   ${({ isPlaying }) => (isPlaying ? zIndex('base') : zIndex('low'))};
   ${({ hasVideo }) =>
     hasVideo ? 'position: absolute; top: 0; left: 0;' : null};
-  @media ${({ theme }) => theme.breakpoint('small')} {
-    position: absolute;
-  }
 `;
 
 const VideoWrapper = styled.div`
