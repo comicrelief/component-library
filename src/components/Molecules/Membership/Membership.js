bdc94bc4ef34bf9e6502c7c6187ac41916efe90b
louiaR <louitiani@gmail.com>
2019-09-30 17:33:20 +0100

    Add money buy box and other amount

--- src/components/Molecules/Membership/Membership.js
+++ src/components/Molecules/Membership/Membership.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 // import PropTypes from 'prop-types';
+import Input from '../../Atoms/Input/Input';
 import MoneyBuy from './MoneyBuy';
+import spacing from '../../../theme/shared/spacing';
+import { media } from '../../../theme/shared/size';
 
 const Wrapper = styled.div`
   max-width: 320px;
 
-  @media ${({ theme }) => theme.breakpoint('small')} {
+  ${media('small')} {
     max-width: 420px;
   }
 `;
@@ -15,15 +18,53 @@ const Form = styled.form`
   display: flex;
   width: 100%;
   justify-content: space-between;
+  flex-direction: column;
+  padding: 0 ${spacing('md')};
+
+  ${media('small')} {
+    padding: 0 ${spacing('l')};
+  }
+`;
+
+const MoneyBuys = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const AmountField = styled(Input)`
+  margin: 0;
+  padding: ${spacing('md')} 0;
+  max-width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  span {
+    flex: 0 0 60%;
+
+    ${media('small')} {
+      flex: 0 0 40%;
+    }
+  }
 `;
 
 const Membership = () => {
   return (
     <Wrapper>
       <Form>
-        <MoneyBuy />
-        <MoneyBuy />
-        <MoneyBuy />
+        <MoneyBuys>
+          <MoneyBuy amount="10" />
+          <MoneyBuy amount="20" />
+          <MoneyBuy amount="30" />
+        </MoneyBuys>
+        <AmountField
+          name="fullname"
+          type="text"
+          label="Other amount"
+          errorMsg=""
+          id="Money buy description"
+          showLabel
+        />
       </Form>
     </Wrapper>
   );
--- src/components/Molecules/Membership/MoneyBuy.js
+++ src/components/Molecules/Membership/MoneyBuy.js
@@ -4,17 +4,22 @@ import styled from 'styled-components';
 
 import Input from '../../Atoms/Input/Input';
 import spacing from '../../../theme/shared/spacing';
+import { media } from '../../../theme/shared/size';
 
 const MoneyBox = styled(Input)`
+  position: relative;
+  font-family: ${({ theme }) => theme.fontFamilies('Montserrat')};
+  font-weight: 800;
+  letter-spacing: -1px;
   display: block;
   margin: 0;
   max-width: 100%;
-  flex: 0 0 32%;
-  padding: ${spacing('md')} ${spacing('xsm')};
+  flex: 0 0 30%;
+  padding: ${spacing('md')} 0;
   cursor: pointer;
 
-  @media ${({ theme }) => theme.breakpoint('small')} {
-    padding: ${spacing('md')} ${spacing('sm')};
+  ${media('small')} {
+    padding: ${spacing('l')} 0;
   }
 `;
 
@@ -32,6 +37,7 @@ const MoneyBuy = ({ amount, currency, description, ...rest }) => {
         errorMsg=""
         id={description}
         showLabel
+        onClick={() => console.log(amount)}
       />
     </>
   );
