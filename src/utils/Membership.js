24bfb5bbdaf4b36c3f1500305b4bf3a4bd7e31d8
louiaR <louitiani@gmail.com>
2019-10-04 10:23:34 +0100

    Helper & prevent characters e, -, +

---
+++ src/utils/Membership.js
@@ -0,0 +1,12 @@
+const onKeyPress = event => {
+  const keyCode = event.keyCode || event.which;
+  const keyValue = String.fromCharCode(keyCode);
+  if (/\+|-|e/.test(keyValue)) event.preventDefault();
+};
+
+const isAmountValid = input => {
+  const reg = /^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$/g;
+  const isValid = (input > 0 || input > 5000) && reg.test(input);
+  return isValid;
+};
+export { onKeyPress, isAmountValid };
