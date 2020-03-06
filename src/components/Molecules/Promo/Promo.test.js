1c1d9fea53d3d3d8a4dfb2fec66b77afb48aeece
Gustavo Liedke <g.liedke@comicrelief.com>
2020-03-06 11:06:50 +0000

    add test

--- src/components/Molecules/Promo/Promo.test.js
+++ src/components/Molecules/Promo/Promo.test.js
@@ -1,125 +1,42 @@
 import React from 'react';
 import 'jest-styled-components';
 import renderWithTheme from '../../../hoc/shallowWithTheme';
-import SingleMessage from './SingleMessage';
+import Promo from './Promo';
 import data from '../../../styleguide/data/data';
 import Text from '../../Atoms/Text/Text';
 import Link from '../../Atoms/Link/Link';
 
 it('renders Single Message with Image correctly', () => {
   const tree = renderWithTheme(
-    <SingleMessage
-      backgroundColor="purple"
-      imageSet={data.images}
-      image={data.image}
+    <Promo
+      backgroundColor="blue_dark"
+      imageSet={data.promoImage}
+      image={data.promoImage}
       imageAltText=""
       copyFirst={false}
+      nudge="bottom"
     >
-      <Text tag="h1" color="white" size="xxl">
-        title
+      <Text
+        tag="h1"
+        color="white"
+        size="super"
+        family="Anton"
+        uppercase
+        weight="normal"
+      >
+        Curabitur pretium tincidunt lacus
       </Text>
       <Text tag="p" color="white">
-        description
+        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam
+        varius, turpis et commodo, est eros bibendum elit, nec luctus magna
+        sollicitudin.
       </Text>
-      <Link href="/" type="button" color="white">
-        CTA
-      </Link>
-    </SingleMessage>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Single Message with no Image correctly', () => {
-  const tree = renderWithTheme(
-    <SingleMessage backgroundColor="purple" copyFirst={false}>
-      <Text tag="p" color="white" size="xxl">
-        “The creativity that goes into helping people have a better life is
-        extraordinary.”
-      </Text>
-    </SingleMessage>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Single Message with full width correctly', () => {
-  const tree = renderWithTheme(
-    <SingleMessage
-      backgroundColor="purple"
-      imageSet={data.images}
-      image={data.image}
-      imageAltText=""
-      copyFirst={false}
-      fullImage
-    >
-      <Text tag="h1" color="white" size="xxl">
-        title
-      </Text>
-      <Text tag="p" color="white">
-        description
-      </Text>
-      <Link href="/" type="button" color="white">
-        CTA
-      </Link>
-    </SingleMessage>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Single Message with full width image and no text correctly', () => {
-  const tree = renderWithTheme(
-    <SingleMessage
-      backgroundColor="purple"
-      imageSet={data.images}
-      image={data.image}
-      imageAltText=""
-      copyFirst={false}
-      fullImage
-    />
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Single Message with 100% vertical height image correctly', () => {
-  const tree = renderWithTheme(
-    <SingleMessage
-      backgroundColor="purple"
-      imageLow={data.imageLow}
-      imageSet={data.images}
-      image={data.image}
-      imageAltText=""
-      vhFull
-    >
-      <Text tag="p" color="white" size="xxl">
-        “The creativity that goes into helping people have a better life is
-        extraordinary.”
-      </Text>
-    </SingleMessage>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Single Message with double image correctly', () => {
-  const tree = renderWithTheme(
-    <SingleMessage
-      backgroundColor="yellow"
-      imageLow={data.imageLow}
-      imageSet={data.images}
-      image={data.image}
-      imageAltText=""
-      imageSet2={data.images}
-      image2={data.image}
-      imageAltText2=""
-    >
-      <Text tag="p" color="black" size="xxl">
-        “The creativity that goes into helping people have a better life is
-        extraordinary.”
-      </Text>
-    </SingleMessage>
+      <span>
+        <Link href="/" type="button" color="white">
+          Show me this
+        </Link>
+      </span>
+    </Promo>
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
