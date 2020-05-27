ae9ef39a5ff07566a9d2633e8aa91bf54a194ed1
Gustavo Liedke <g.liedke@comicrelief.com>
2020-05-27 09:05:15 +0100

    add new logos compo

---
+++ src/components/Molecules/Logos/Logos.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Logo from '../../Atoms/Logo/Logo';
+
+const Logos = ({ campaign }) => {
+  if (campaign === 'Sport Relief Gameon') {
+    return (
+      <>
+        <a href="/" title="Go to Comic Relief homepage">
+          <Logo rotate sizeSm="50px" sizeMd="60px" campaign="Comic Relief" />
+        </a>
+        <a href="/sportrelief/" title="Go to Sport Relief homepage">
+          <Logo sizeSm="100px" sizeMd="120px" campaign="Sport Relief Gameon" />
+        </a>
+      </>
+    );
+  }
+
+  if (campaign === 'Big Night In') {
+    return (
+      <a href="/big-night-in" title="Big night in homepage">
+        <Logo rotate="false" campaign="Big Night In" />
+      </a>
+    );
+  }
+
+  if (campaign === 'Sport Relief') {
+    return (
+      <a href="/sportrelief" title="Sport Relief in homepage">
+        <Logo rotate="true" campaign="Sport Relief" />
+      </a>
+    );
+  }
+
+  return (
+    <a href="/" title="Go to Comic Relief homepage">
+      <Logo rotate="true" campaign="Comic Relief" />
+    </a>
+  );
+};
+
+Logos.propTypes = {
+  campaign: PropTypes.string
+};
+
+Logos.defaultProps = {
+  campaign: 'Comic Relief'
+};
+
+export default Logos;
