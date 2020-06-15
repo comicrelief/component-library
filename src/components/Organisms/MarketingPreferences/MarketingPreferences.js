16a8c4e57872205b2066f7c8817f2792f7f57e63
tomisaacmarshall <thomasisaacmarshall@gmail.com>
2020-06-15 12:49:54 +0100

    Update marketing pref field names to match the service & add prefix prop

--- src/components/Organisms/MarketingPreferences/MarketingPreferences.js
+++ src/components/Organisms/MarketingPreferences/MarketingPreferences.js
@@ -7,13 +7,13 @@ import InputController from './InputController';
 import CheckAnswer from './CheckAnswer';
 import { CopyWrapper, Head, Body, Field } from './MarketingPreferences.style';
 
-const MarketingPreferences = ({ copyTop, copyBottom }) => {
+const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
   const { control, errors, watch, register } = useFormContext();
 
-  const watchEmail = watch('mpEmail', 'no');
-  const watchAddress = watch('mpAddress', 'no');
-  const watchPhone = watch('mpTelephone', 'no');
-  const watchMobile = watch('mpMobilePhone', 'no');
+  const watchEmail = watch(`${fieldPrefix}permissionEmail`, 'no');
+  const watchAddress = watch(`${fieldPrefix}permissionPost`, 'no');
+  const watchPhone = watch(`${fieldPrefix}permissionPhone`, 'no');
+  const watchMobile = watch(`${fieldPrefix}permissionSMS`, 'no');
 
   return (
     <>
@@ -23,7 +23,10 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           <Text tag="h3" size="l" family="Anton" uppercase weight="400">
             Email
           </Text>
-          <CheckAnswer name="mpEmail" register={register} />
+          <CheckAnswer
+            name={`${fieldPrefix}permissionEmail`}
+            register={register}
+          />
         </Head>
         <Body>
           <Text tag="p" color="grey">
@@ -33,7 +36,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           {watchEmail === 'yes' && (
             <InputController
               placeholder="Email Address"
-              fieldName="mpEmailAddress"
+              fieldName={`${fieldPrefix}email`}
               label="Email Address"
               rules={{ required: 'This is required.' }}
               control={control}
@@ -47,7 +50,10 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           <Text tag="h3" size="l" family="Anton" uppercase weight="400">
             Post
           </Text>
-          <CheckAnswer name="mpAddress" register={register} />
+          <CheckAnswer
+            name={`${fieldPrefix}permissionPost`}
+            register={register}
+          />
         </Head>
         <Body>
           <Text tag="p" color="grey">
@@ -58,7 +64,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
             <>
               <InputController
                 placeholder="Address Line 1"
-                fieldName="mpAddress1"
+                fieldName={`${fieldPrefix}address1`}
                 label="Address Line 1"
                 rules={{ required: 'This is required.' }}
                 control={control}
@@ -66,21 +72,21 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
               />
               <InputController
                 placeholder="Address Line 2"
-                fieldName="mpAddress2"
+                fieldName={`${fieldPrefix}address2`}
                 label="Address Line 2"
                 control={control}
                 errors={errors}
               />
               <InputController
                 placeholder="Address Line 3"
-                fieldName="mpAddress3"
+                fieldName={`${fieldPrefix}address3`}
                 label="Address Line 3"
                 control={control}
                 errors={errors}
               />
               <InputController
                 placeholder="Town/City"
-                fieldName="mpTownCity"
+                fieldName={`${fieldPrefix}town`}
                 label="Town/City"
                 rules={{ required: 'This is required.' }}
                 control={control}
@@ -88,7 +94,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
               />
               <InputController
                 placeholder="Postcode"
-                fieldName="mpPostcode"
+                fieldName={`${fieldPrefix}postcode`}
                 label="Postcode"
                 rules={{ required: 'This is required.' }}
                 control={control}
@@ -96,7 +102,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
               />
               <InputController
                 placeholder="Country"
-                fieldName="mpCountry"
+                fieldName={`${fieldPrefix}country`}
                 label="Country"
                 rules={{ required: 'This is required.' }}
                 control={control}
@@ -111,7 +117,10 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           <Text tag="h3" size="l" family="Anton" uppercase weight="400">
             Phone
           </Text>
-          <CheckAnswer name="mpTelephone" register={register} />
+          <CheckAnswer
+            name={`${fieldPrefix}permissionPhone`}
+            register={register}
+          />
         </Head>
         <Body>
           <Text tag="p" color="grey">
@@ -121,7 +130,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           {watchPhone === 'yes' && (
             <InputController
               placeholder="Phone Number"
-              fieldName="mpPhone"
+              fieldName={`${fieldPrefix}phone`}
               label="Phone Number"
               rules={{ required: 'This is required.' }}
               control={control}
@@ -135,7 +144,10 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           <Text tag="h3" size="l" family="Anton" uppercase weight="400">
             SMS
           </Text>
-          <CheckAnswer name="mpMobilePhone" register={register} />
+          <CheckAnswer
+            name={`${fieldPrefix}permissionSMS`}
+            register={register}
+          />
         </Head>
         <Body>
           <Text tag="p" color="grey">
@@ -145,7 +157,7 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
           {watchMobile === 'yes' && (
             <InputController
               placeholder="Mobile Number"
-              fieldName="mpMobile"
+              fieldName={`${fieldPrefix}mobile`}
               label="Mobile Number"
               rules={{ required: 'This is required.' }}
               control={control}
@@ -161,12 +173,15 @@ const MarketingPreferences = ({ copyTop, copyBottom }) => {
 
 MarketingPreferences.propTypes = {
   copyTop: PropTypes.node,
-  copyBottom: PropTypes.node
+  copyBottom: PropTypes.node,
+  /** Can be used to prevent clashes with other fields if this component is being used in a larger form. */
+  fieldPrefix: PropTypes.string
 };
 
 MarketingPreferences.defaultProps = {
   copyTop: null,
-  copyBottom: null
+  copyBottom: null,
+  fieldPrefix: ''
 };
 
 export default MarketingPreferences;
