ac1021c079b47e9e43fed7c212de8cd8b3c29597
tomisaacmarshall <thomasisaacmarshall@gmail.com>
2020-06-18 16:40:41 +0100

    fix: marketing preference inputs can be disabled

--- src/components/Organisms/MarketingPreferences/MarketingPreferences.js
+++ src/components/Organisms/MarketingPreferences/MarketingPreferences.js
@@ -15,7 +15,15 @@ const ShowHide = styled.div`
   display: ${({ show }) => (show ? 'block' : 'none')};
 `;
 
-const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
+const MarketingPreferences = ({
+  copyTop,
+  copyBottom,
+  fieldPrefix,
+  disableEmailInput,
+  disablePostInputs,
+  disablePhoneInput,
+  disableSMSInput
+}) => {
   const { watch } = useFormContext();
   const prefixName = name => fieldPrefix + name;
 
@@ -34,20 +42,22 @@ const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
           </Text>
           <CheckAnswer name={prefixName('permissionEmail')} />
         </Head>
-        <Body>
-          <ShowHide show={watchEmail}>
-            {watchEmail === 'no' && (
-              <Text tag="p" size="s">
-                Enter email you wish us to remove from our list
-              </Text>
-            )}
-            <InputController
-              placeholder="Email Address"
-              fieldName={prefixName('email')}
-              label="Email Address"
-            />
-          </ShowHide>
-        </Body>
+        <MaybeDisabled disabled={disableEmailInput}>
+          <Body>
+            <ShowHide show={watchEmail}>
+              {watchEmail === 'no' && (
+                <Text tag="p" size="s">
+                  Enter email you wish us to remove from our list
+                </Text>
+              )}
+              <InputController
+                placeholder="Email Address"
+                fieldName={prefixName('email')}
+                label="Email Address"
+              />
+            </ShowHide>
+          </Body>
+        </MaybeDisabled>
       </Field>
       <Field>
         <Head>
@@ -56,45 +66,47 @@ const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
           </Text>
           <CheckAnswer name={prefixName('permissionPost')} />
         </Head>
-        <Body>
-          <ShowHide show={watchAddress}>
-            {watchAddress === 'no' && (
-              <Text tag="p" size="s">
-                Enter address you wish us to remove from our list
-              </Text>
-            )}
-            <InputController
-              placeholder="Address Line 1"
-              fieldName={prefixName('address1')}
-              label="Address Line 1"
-            />
-            <InputController
-              placeholder="Address Line 2"
-              fieldName={prefixName('address2')}
-              label="Address Line 2"
-            />
-            <InputController
-              placeholder="Address Line 3"
-              fieldName={prefixName('address3')}
-              label="Address Line 3"
-            />
-            <InputController
-              placeholder="Town/City"
-              fieldName={prefixName('town')}
-              label="Town/City"
-            />
-            <InputController
-              placeholder="Postcode"
-              fieldName={prefixName('postcode')}
-              label="Postcode"
-            />
-            <InputController
-              placeholder="Country"
-              fieldName={prefixName('country')}
-              label="Country"
-            />
-          </ShowHide>
-        </Body>
+        <MaybeDisabled disabled={disablePostInputs}>
+          <Body>
+            <ShowHide show={watchAddress}>
+              {watchAddress === 'no' && (
+                <Text tag="p" size="s">
+                  Enter address you wish us to remove from our list
+                </Text>
+              )}
+              <InputController
+                placeholder="Address Line 1"
+                fieldName={prefixName('address1')}
+                label="Address Line 1"
+              />
+              <InputController
+                placeholder="Address Line 2"
+                fieldName={prefixName('address2')}
+                label="Address Line 2"
+              />
+              <InputController
+                placeholder="Address Line 3"
+                fieldName={prefixName('address3')}
+                label="Address Line 3"
+              />
+              <InputController
+                placeholder="Town/City"
+                fieldName={prefixName('town')}
+                label="Town/City"
+              />
+              <InputController
+                placeholder="Postcode"
+                fieldName={prefixName('postcode')}
+                label="Postcode"
+              />
+              <InputController
+                placeholder="Country"
+                fieldName={prefixName('country')}
+                label="Country"
+              />
+            </ShowHide>
+          </Body>
+        </MaybeDisabled>
       </Field>
       <Field>
         <Head>
@@ -103,20 +115,22 @@ const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
           </Text>
           <CheckAnswer name={prefixName('permissionPhone')} />
         </Head>
-        <Body>
-          <ShowHide show={watchPhone}>
-            {watchPhone === 'no' && (
-              <Text tag="p" size="s">
-                Enter phone number you wish us to remove from our list
-              </Text>
-            )}
-            <InputController
-              placeholder="Phone Number"
-              fieldName={prefixName('phone')}
-              label="Phone Number"
-            />
-          </ShowHide>
-        </Body>
+        <MaybeDisabled disabled={disablePhoneInput}>
+          <Body>
+            <ShowHide show={watchPhone}>
+              {watchPhone === 'no' && (
+                <Text tag="p" size="s">
+                  Enter phone number you wish us to remove from our list
+                </Text>
+              )}
+              <InputController
+                placeholder="Phone Number"
+                fieldName={prefixName('phone')}
+                label="Phone Number"
+              />
+            </ShowHide>
+          </Body>
+        </MaybeDisabled>
       </Field>
       <Field>
         <Head>
@@ -125,20 +139,22 @@ const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
           </Text>
           <CheckAnswer name={prefixName('permissionSMS')} />
         </Head>
-        <Body>
-          <ShowHide show={watchMobile}>
-            {watchMobile === 'no' && (
-              <Text tag="p" size="s">
-                Enter mobile number you wish us to remove from our list
-              </Text>
-            )}
-            <InputController
-              placeholder="Mobile Number"
-              fieldName={prefixName('mobile')}
-              label="Mobile Number"
-            />
-          </ShowHide>
-        </Body>
+        <MaybeDisabled disabled={disableSMSInput}>
+          <Body>
+            <ShowHide show={watchMobile}>
+              {watchMobile === 'no' && (
+                <Text tag="p" size="s">
+                  Enter mobile number you wish us to remove from our list
+                </Text>
+              )}
+              <InputController
+                placeholder="Mobile Number"
+                fieldName={prefixName('mobile')}
+                label="Mobile Number"
+              />
+            </ShowHide>
+          </Body>
+        </MaybeDisabled>
       </Field>
       {copyBottom && <CopyWrapper>{copyBottom}</CopyWrapper>}
     </>
@@ -152,17 +168,32 @@ const MarketingPreferences = ({ copyTop, copyBottom, fieldPrefix }) => {
 //   </Button>
 // );
 
+const MaybeDisabled = ({ children, disabled }) => {
+  if (disabled) {
+    return null;
+  }
+  return children;
+};
+
 MarketingPreferences.propTypes = {
   copyTop: PropTypes.node,
   copyBottom: PropTypes.node,
   /** Can be used to prevent clashes with other fields if this component is being used in a larger form. */
-  fieldPrefix: PropTypes.string
+  fieldPrefix: PropTypes.string,
+  disableEmailInput: PropTypes.bool,
+  disablePostInputs: PropTypes.bool,
+  disablePhoneInput: PropTypes.bool,
+  disableSMSInput: PropTypes.bool
 };
 
 MarketingPreferences.defaultProps = {
   copyTop: null,
   copyBottom: null,
-  fieldPrefix: ''
+  fieldPrefix: '',
+  disableEmailInput: false,
+  disablePostInputs: false,
+  disablePhoneInput: false,
+  disableSMSInput: false
 };
 
 export default MarketingPreferences;
