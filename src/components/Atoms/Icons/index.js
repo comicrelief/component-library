6c22bab25eb5ebe4de5a91bab50aaf79b13ac6f2
Gustavo Liedke <g.liedke@comicrelief.com>
2020-06-22 12:31:06 +0100

    revert favourite export

--- src/components/Atoms/Icons/index.js
+++ src/components/Atoms/Icons/index.js
@@ -1,5 +1,4 @@
 export { default as External } from './External';
 export { default as Download } from './Download';
-// todo: disabled due memory leak issue
-// export { default as Favourite } from './Favourite';
+export { default as Favourite } from './Favourite';
 export { default as Internal } from './Internal';
