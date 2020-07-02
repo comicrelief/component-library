25dbd00992f87c7aae050b9069784f562b71a84b
louiaR <louitiani@gmail.com>
2020-07-02 16:51:20 +0100

    Add assets , improve style & functionality

--- src/components/Molecules/Chips/Chips.js
+++ src/components/Molecules/Chips/Chips.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import Text from '../../Atoms/Text/Text';
 import spacing from '../../../theme/shared/spacing';
 import zIndex from '../../../theme/shared/zIndex';
 import media from '../../../theme/shared/breakpoint';
+import redShed from './assets/CR_RedShed.svg';
+import howWeFund from './assets/CR_Funding.svg';
+import mentalHealth from './assets/CR_MentalHealth.svg';
+import socialTech from './assets/CR_SocialTech.svg';
+import sportForChange from './assets/CR_Sport.svg';
+import genderJustice from './assets/CR_Justice.svg';
+import safePlaceToBe from './assets/CR_SafePlace.svg';
+import partnerships from './assets/CR_Partnerships.svg';
+import childrenThrive from './assets/CR_Children.svg';
+import clear from './assets/clear.svg';
+
+const selectIcon = name => {
+  switch (name) {
+    case 'How we Fund':
+      return howWeFund;
+    case 'Red Shed':
+      return redShed;
+    case 'Mental Health':
+      return mentalHealth;
+    case 'Social Tech':
+      return socialTech;
+    case 'Gender Justice':
+      return genderJustice;
+    case 'Sport for Change':
+      return sportForChange;
+    case 'Safe Place to Be':
+      return safePlaceToBe;
+    case 'Partnerships':
+      return partnerships;
+    case 'Children Survive & Thrive':
+      return childrenThrive;
+    default:
+      return null;
+  }
+};
 
 const LabelGroup = styled.div`
   display: flex;
@@ -56,6 +93,7 @@ const CheckInput = styled.input`
   margin: 0;
   border: 1px solid ${({ theme }) => theme.color('grey_for_forms')};
   + span {
+    background: url(${({ value }) => selectIcon(value)}) no-repeat center;
     background-size: 24px;
     margin-right: ${spacing('md')};
     width: 40px;
@@ -79,13 +117,38 @@ const CheckInput = styled.input`
   }
 `;
 
-const Chips = categories => {
+const ClearButton = styled.button`
+  border-radius: 2rem;
+  background: none;
+  padding: ${spacing('sm')} ${spacing('md')};
+  margin: ${spacing('md')} 0 0 0;
+  border: 1px solid ${({ theme }) => theme.color('grey_medium')};
+  outline: none;
+  padding-left: 3rem;
+  position: relative;
+  height: 48px;
+  :focus {
+    border-color: ${({ theme }) => theme.color('grey_for_forms')};
+  }
+
+  :before {
+    content: '';
+    background: url(${clear}) no-repeat center;
+    width: 20px;
+    height: 20px;
+    display: block;
+    position: absolute;
+    left: 1rem;
+  }
+`;
+
+const Chips = ({ categories, handleToggle, selected, resetSelection }) => {
   return (
     <LabelGroup>
-      {categories.categories.map(category => (
+      {categories.map(category => (
         <CheckLabel key={category.id}>
           <CheckInput
-            onChange={() => console.log('function')}
+            onChange={handleToggle}
             type="checkbox"
             checked={category.checked}
             value={category.fieldValue}
@@ -95,8 +158,20 @@ const Chips = categories => {
           {category.fieldValue}
         </CheckLabel>
       ))}
+      {selected.length !== 0 && (
+        <ClearButton onClick={resetSelection} type="button">
+          <Text size="s">Clear selection</Text>
+        </ClearButton>
+      )}
     </LabelGroup>
   );
+};
+
+Chips.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  handleToggle: PropTypes.func.isRequired,
+  resetSelection: PropTypes.func.isRequired,
+  selected: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Chips;
--- src/components/Molecules/Chips/Chips.md
+++ src/components/Molecules/Chips/Chips.md
@@ -1,10 +1,51 @@
 # Chips
 
 ```js
+const group = [
+  { fieldValue: 'Gender Justice' },
+  { fieldValue: 'How we Fund' },
+  { fieldValue: 'Partnerships' },
+  { fieldValue: 'Red Shed' },
+  { fieldValue: 'Social Tech' },
+  { fieldValue: 'Sport for Change' }
+];
+
+const newGroup = group.map((cats, index) => ({
+  id: index,
+  ...cats,
+  checked: false
+}));
+
+const [categories, setCategories] = React.useState(newGroup);
+const [selected, setSelected] = React.useState([]);
+
+const handleToggle = e => {
+  const result = categories.map(cat => {
+    const newCat = { ...cat };
+    if (newCat.fieldValue === e.target.value) {
+      newCat.checked = !newCat.checked;
+    }
+    return newCat;
+  });
+  setCategories(result);
+};
+
+React.useEffect(() => {
+  const filtered = categories
+    .map(cat => {
+      if (cat.checked === true) {
+        return cat.fieldValue;
+      }
+      return null;
+    })
+    .filter(el => el != null);
+  console.log(filtered);
+  setSelected(filtered);
+}, [categories]);
+
 <Chips
-  categories={[
-    { id: '2', fieldValue: 'Gender Justice', checked: false },
-    { id: '2', fieldValue: 'Gender Justice', checked: false }
-  ]}
-/>
+  categories={categories}
+  handleToggle={handleToggle}
+  selected={selected}
+/>;
 ```
