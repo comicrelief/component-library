7191adcb63b7f4829a7a3bf8cffe28e40f6dc25e
louiaR <louitiani@gmail.com>
2020-08-06 10:54:51 +0100

    Style & add props title

--- src/components/Molecules/Accordion/Accordion.js
+++ src/components/Molecules/Accordion/Accordion.js
@@ -2,17 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import spacing from '../../../theme/shared/spacing';
+import Text from '../../Atoms/Text/Text';
+
 const Container = styled.div`
   display: flex;
   position: relative;
   flex-direction: column;
   height: 100%;
   background: ${({ theme, backgroundColor }) => theme.color(backgroundColor)};
+  padding: calc(${spacing('md')} + ${spacing('m')}) calc(${spacing('m')} * 2);
+`;
+
+const Title = styled(Text)`
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  margin: 0 0 ${spacing('md')};
 `;
 
 const Accordion = ({ children, title }) => (
   <Container>
-    <h1>{title}</h1>
+    <Title tag="h6" family="Anton" weight="normal">{title}</Title>
     {children}
   </Container>
 );
