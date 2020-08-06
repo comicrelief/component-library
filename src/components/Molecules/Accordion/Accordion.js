bae78bc1293d3108277ef77272b612a0bf84d08b
louiaR <louitiani@gmail.com>
2020-08-06 12:28:04 +0100

    Add open/close functionality & improve style

--- src/components/Molecules/Accordion/Accordion.js
+++ src/components/Molecules/Accordion/Accordion.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -6,6 +6,7 @@ import spacing from '../../../theme/shared/spacing';
 import Text from '../../Atoms/Text/Text';
 
 const Container = styled.div`
+  position: relative;
   display: flex;
   position: relative;
   flex-direction: column;
@@ -14,18 +15,38 @@ const Container = styled.div`
   padding: calc(${spacing('md')} + ${spacing('m')}) calc(${spacing('m')} * 2);
 `;
 
+const Icon = styled.div`
+  cursor: pointer;
+  position: absolute;
+  bottom: -10px;
+  right: calc(${spacing('m')} * 2);
+`;
+
 const Title = styled(Text)`
   letter-spacing: 0.03em;
   text-transform: uppercase;
   margin: 0 0 ${spacing('md')};
 `;
 
-const Accordion = ({ children, title }) => (
-  <Container>
-    <Title tag="h6" family="Anton" weight="normal">{title}</Title>
-    {children}
-  </Container>
-);
+const Copy = styled.div`
+  margin: ${spacing('l')} 0 ${spacing('m')};
+  ${({ isOpen }) => (isOpen ? 'display: block;' : 'display: none;')}
+`;
+
+const Accordion = ({ children, title }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Container>
+      <Title tag="h6" family="Anton" weight="normal">{title}</Title>
+      <Copy isOpen={isOpen}>{children}</Copy>
+      <Icon onClick={handleOpen}>⬆</Icon>
+    </Container>
+  );
+};
 
 Accordion.propTypes = {
   children: PropTypes.node.isRequired,
