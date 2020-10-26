6c9544ca72dd5a2df86452c4857c76bfc8983d2e
Gustavo Liedke <contact@gustavoliedke.com>
2020-10-26 14:41:25 +0000

    revert tests

--- src/components/Atoms/Select/Select.test.js
+++ src/components/Atoms/Select/Select.test.js
@@ -1,25 +1,25 @@
-// import React from 'react';
+import React from 'react';
 import 'jest-styled-components';
-// import renderWithTheme from '../../../hoc/shallowWithTheme';
-// import Select from './Select';
+import renderWithTheme from '../../../hoc/shallowWithTheme';
+import Select from './Select';
 
 it('renders correctly', () => {
-  // const tree = renderWithTheme(
-  //   <Select
-  //     label="Label"
-  //     description="Please choose an option"
-  //     errorMsg=""
-  //     options={[
-  //       { value: 'Option one', displayValue: 'The first option' },
-  //       {
-  //         value: 'Option two',
-  //         displayValue: 'The second option'
-  //       },
-  //       { value: 'Option three', displayValue: 'The third option' },
-  //       { value: 'Option four', displayValue: 'The fourth option' }
-  //     ]}
-  //   />
-  // ).toJSON();
+  const tree = renderWithTheme(
+    <Select
+      label="Label"
+      description="Please choose an option"
+      errorMsg=""
+      options={[
+        { value: 'Option one', displayValue: 'The first option' },
+        {
+          value: 'Option two',
+          displayValue: 'The second option'
+        },
+        { value: 'Option three', displayValue: 'The third option' },
+        { value: 'Option four', displayValue: 'The fourth option' }
+      ]}
+    />
+  ).toJSON();
 
-  // expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
--- src/components/Organisms/Footer/Footer.test.js
+++ src/components/Organisms/Footer/Footer.test.js
@@ -1,16 +1,16 @@
 /* eslint-disable react/jsx-wrap-multilines */
-// import React from 'react';
+import React from 'react';
 import 'jest-styled-components';
-// import renderWithTheme from '../../../hoc/shallowWithTheme';
-// import Footer from './Footer';
+import renderWithTheme from '../../../hoc/shallowWithTheme';
+import Footer from './Footer';
 
-// import data from './data/data';
-// import footerCopy from './data/footerCopy';
+import data from './data/data';
+import footerCopy from './data/footerCopy';
 
 it('renders correctly', () => {
-  // const tree = renderWithTheme(
-  //   <Footer navItems={data} footerCopy={footerCopy.copy} />
-  // ).toJSON();
+  const tree = renderWithTheme(
+    <Footer navItems={data} footerCopy={footerCopy.copy} />
+  ).toJSON();
 
-  // expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
