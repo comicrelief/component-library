2dbeb42fefbfc9a79d4f7f5b6d748c3698e225fc
Gustavo Liedke <contact@gustavoliedke.com>
2020-12-07 10:23:17 +0000

    fix eslints

--- src/components/Molecules/Countdown/Countdown.js
+++ src/components/Molecules/Countdown/Countdown.js
@@ -3,34 +3,33 @@ import PropTypes from 'prop-types';
 import Text from '../../Atoms/Text/Text';
 import { Wrapper, Digits } from './Countdown.style';
 
-
-const Countdown = ({endDate, color, endMessage, introMessage}) => {
+const Countdown = ({
+  endDate, color, endMessage, introMessage
+}) => {
   const [countdownEnd, setCountdownEnd] = useState(false);
   const [countdownTime, setCountdownTime] = useState({});
-  
+
   const countDownDate = new Date(endDate).getTime();
   // Keep number with two digits
-  const formatNumber = number => {
-    return ("0" + number).slice(-2);
-  }
+  const formatNumber = number => (`0${number}`).slice(-2);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
-  
+
       const days = formatNumber(Math.floor(distance / (1000 * 60 * 60 * 24)));
       const hours = formatNumber(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
       const minutes = formatNumber(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
       const seconds = formatNumber(Math.floor((distance % (1000 * 60)) / 1000));
-  
+
       setCountdownTime({
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds
+        days,
+        hours,
+        minutes,
+        seconds
       });
-  
+
       if (distance < 0) {
         setCountdownEnd(true);
       }
@@ -83,7 +82,7 @@ const Countdown = ({endDate, color, endMessage, introMessage}) => {
       </Wrapper>
     </>
   );
-  
+
   if (endMessage == null) {
     return !countdownEnd ? CountDownClock : null;
   }
@@ -96,12 +95,12 @@ Countdown.protoTypes = {
   color: PropTypes.string,
   endMessage: PropTypes.node,
   introMessage: PropTypes.node
-}
+};
 
 Countdown.defaultProps = {
-  color: "black",
+  color: 'black',
   endMessage: null,
   introMessage: null
-}
+};
 
 export default Countdown;
