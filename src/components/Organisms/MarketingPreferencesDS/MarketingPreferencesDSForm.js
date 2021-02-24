232b377220186acdd2517455e8de6db08bb581e5
Andy E Phipps <a.phipps@comicrelief.com>
2021-02-24 11:30:45 +0000

    Set-up customised example

--- src/components/Organisms/MarketingPreferencesDS/MarketingPreferencesDSForm.js
+++ src/components/Organisms/MarketingPreferencesDS/MarketingPreferencesDSForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import Text from '../../Atoms/Text/Text';
 
 import {
   MarketingPreferencesDS,
@@ -8,13 +9,32 @@ import {
   buildValidationSchema
 } from './_MarketingPreferencesDS';
 
+/**
+ * Normal usage would be a single instance of the Marketing Preferences, but we're creating two here
+ * to illustrate the difference in config options; see _MarketingPrefsConfig.js for more details.
+ */
+
+/* Create default validation and initial values by not passing any override
+ parameters into the functions: */
 const mpValidation = buildValidationSchema();
 const initialValues = setInitialValues();
+const { mpValidationSchema, mpValidationOptions } = mpValidation;
+
+// Or customise and override the config to suit the destination app's requirements:
+const initalValueOverrides = { mp_email: 'user@website.com' }; // Potentially provided earlier in the journey
+const validationOverrides = {
+  mp_permissionSMS: { disableOption: true }, // Remove this option entirely
+  mp_permissionPost: { no: false } // Make Post fields non-req (so not shown) when 'No' selected
+};
+
+// Pass these override objects in as parameters
+const mpValidationCustom = buildValidationSchema(validationOverrides);
+const initialValuesCustom = setInitialValues(initalValueOverrides);
 
 const {
-  mpValidationSchema,
-  mpValidationOptions
-} = mpValidation;
+  mpValidationSchema: mpValidationSchemaCustom,
+  mpValidationOptions: mpValidationOptionsCustom
+} = mpValidationCustom;
 
 const MarketingPreferencesDSForm = () => {
   function customSubmitHandler() {
@@ -22,26 +42,47 @@ const MarketingPreferencesDSForm = () => {
     console.log('customSubmitHandler');
   }
 
+  // For our default instance:
   const formMethods = useForm({
     mode: 'onBlur',
     reValidateMode: 'onBlur',
     defaultValues: initialValues,
     resolver: yupResolver(mpValidationSchema)
   });
-
   const { handleSubmit } = formMethods;
+
+  // For our customised-instance:
+  const formMethodsCustom = useForm({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    defaultValues: initialValuesCustom,
+    resolver: yupResolver(mpValidationSchemaCustom)
+  });
+  const { handleSubmit: handleSubmitCustom } = formMethodsCustom;
 
   return (
     <>
+      <Text>Our default instance:</Text>
       <FormProvider {...formMethods}>
         <form onSubmit={handleSubmit(customSubmitHandler)} noValidate>
-          <input type="submit" />
           <MarketingPreferencesDS
             mpValidationOptions={mpValidationOptions}
           />
+          <input type="submit" />
         </form>
       </FormProvider>
 
+      <Text tag="h3" size="xl">- - - - - - - - - - - - </Text>
+
+      <Text>Our customised instance:</Text>
+      <FormProvider {...formMethodsCustom}>
+        <form onSubmit={handleSubmitCustom(customSubmitHandler)} noValidate>
+          <MarketingPreferencesDS
+            mpValidationOptions={mpValidationOptionsCustom}
+          />
+          <input type="submit" />
+        </form>
+      </FormProvider>
     </>
   );
 };
