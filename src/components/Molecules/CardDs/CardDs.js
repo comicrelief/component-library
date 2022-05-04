e69e800e459eaf0fee31b4596c0b78c4dac704b2
michael o'donovan <mikeodonovan3@gmail.com>
2022-05-04 17:13:42 +0100

    refactor(CardsDs.js): Remove code that doesn't do anything, but is simply wrapped in a media query

--- src/components/Molecules/CardDs/CardDs.js
+++ src/components/Molecules/CardDs/CardDs.js
@@ -26,9 +26,6 @@ const Image = styled.div`
   height: auto;
   margin: 0 -${spacing('m')} 0 ${spacing('m')};
 
-  @media ${({ theme }) => theme.breakpoint('large')} {
-    margin: 0 -${spacing('m')} 0 ${spacing('m')};
-  }
   img {
     border-radius: 1rem;
     box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15);
