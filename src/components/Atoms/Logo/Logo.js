e61b32e2fa24682534bc42ebadc444c077e45982
Andy E Phipps <a.phipps@comicrelief.com>
2022-08-25 11:05:18 +0100

    Keeping naming convention

--- src/components/Atoms/Logo/Logo.js
+++ src/components/Atoms/Logo/Logo.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import zIndex from '../../../theme/shared/zIndex';
 import spacing from '../../../theme/shared/spacing';
-import crLogo from './assets/CRlogo-140px.png';
+import crLogo from './assets/cr-logo.png';
 import srLogo from './assets/sr-logo.svg';
 import srLogoGameOn from './assets/sr-gameon-logo.svg';
 
