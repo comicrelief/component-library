fdbf55b13bec967e70fc3c63c3fc87c7bff13cbc
Micheal O'Donovan <mikeodonovan3@gmail.com>
2022-09-30 16:52:20 +0100

    make Cross use more CL values

--- src/components/Organisms/Donate/Form/PopUpComponent.js
+++ src/components/Organisms/Donate/Form/PopUpComponent.js
@@ -6,7 +6,8 @@ import spacing from '../../../../theme/shared/spacing';
 import { media } from '../../../../theme/shared/size';
 
 const StyledPopUp = styled.div`
-  display: ${props => props.isPop ? "block" : "none"};
+  display: ${props => props.isPop ? "grid" : "none"};
+  gap: ${spacing('sm')};
   background-color: ${({ theme }) => theme.color('white')};
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.15);
   border-radius: 8px;
@@ -20,8 +21,7 @@ const StyledPopUp = styled.div`
 `;
 
 const Cross = styled.div`
-  text-align: right;
-  font-size: 22px;
+  justify-self: end;
 `;
 
 const PopUpComponent = () => {
@@ -29,7 +29,9 @@ const PopUpComponent = () => {
 
   return (
     <StyledPopUp isPop={isPop}>
-      <Cross onClick={() => setIsPop(false)}>x</Cross>
+      <Cross onClick={() => setIsPop(false)}>
+        <Text weight="bold" size="m">x</Text>
+      </Cross>
       <Text>
         We appreciate all of the donations we recieve. You can make a bigger impact by donating a smaller amount every month, to help people live free from poverty, violence and discrimination both in the UK and around the world.
       </Text>
