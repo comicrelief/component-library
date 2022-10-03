f78adf5045f8540560c0788c7f31dd580d83c21c
Micheal O'Donovan <mikeodonovan3@gmail.com>
2022-10-03 14:54:36 +0100

    take things back to before failed attempt to fix tests

--- src/components/Molecules/Box/Box.test.js
+++ src/components/Molecules/Box/Box.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import 'jest-styled-components';
 import renderWithTheme from '../../../hoc/shallowWithTheme';
 import Box from './Box';
-import { desktopImages } from '../../../styleguide/data/data';
+import data from '../../../styleguide/data/data';
 
 it('renders correctly', () => {
   const tree = renderWithTheme(
-    <Box imageLow={desktopImages.image} images={desktopImages.image}>
+    <Box imageLow={data.image} images={data.image}>
       <h2>Title</h2>
       <p>Description</p>
     </Box>
