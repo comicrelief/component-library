4bb8eafe2088e832cce39e4d0698f032ae466db2
AndyEPhipps <a.phipps@comicrelief.com>
2022-10-05 16:23:51 +0100

    Properly pass transparent colour

--- src/theme/crTheme/colors.js
+++ src/theme/crTheme/colors.js
@@ -44,7 +44,9 @@ const colors = {
   white: '#FFFFFF',
   yellow: '#FFE400',
   yellow_dark: '#665700',
-  yellow_light: '#FEFD5A'
+  yellow_light: '#FEFD5A',
+  Transparent: 'transparent',
+  transparent: 'transparent'
 };
 
 export default colorName => {
