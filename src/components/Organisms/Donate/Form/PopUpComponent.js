36c134eaa84cb449c68b7e85745a23a01646c228
Micheal O'Donovan <mikeodonovan3@gmail.com>
2022-10-06 13:37:29 +0100

    feat: Change colour of pop-up text box to light blue

--- src/components/Organisms/Donate/Form/PopUpComponent.js
+++ src/components/Organisms/Donate/Form/PopUpComponent.js
@@ -2,14 +2,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Text from '../../../Atoms/Text/Text';
-import spacing from '../../../../theme/shared/spacing';
 import { media } from '../../../../theme/shared/size';
 import PropTypes from 'prop-types';
 import CloseCross from '../assets/close.svg';
 
 const StyledPopUp = styled.div`
   display: ${props => props.isPop ? "grid" : "none"};
-  background-color: ${({ theme }) => theme.color('white')};
+  background-color: ${({ theme }) => theme.color('blue_light')};
+  /* blue_royal */
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.15);
   border-radius: 8px;
 
@@ -26,10 +26,9 @@ const TextWrapper = styled.div`
 
 const Button = styled.button`
   justify-self: end;
-  background: transparent;
+  background-color: transparent;
   border: 0;
   cursor: pointer;
-  border: 1px solid ${({ theme }) => theme.color('white')};
   :active,
   :focus,
   :hover {
