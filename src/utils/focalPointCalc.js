0528136787b69d7759090ba5dc6cdcc69e80a2cc
AndyEPhipps <a.phipps@comicrelief.com>
2022-10-13 10:03:03 +0100

    Better comments

--- src/utils/focalPointCalc.js
+++ src/utils/focalPointCalc.js
@@ -1,18 +1,18 @@
-// A handy to make the percentage math nice and reusable
+/* A handy to make the percentage math nice and reusable */
 const focalPointCalc = focalPointData => {
-  // Calculate the focal point as a percentage of the image dimension
+  // Calculate the focal points as percentages of the image dimensions
   let x = Math.round((focalPointData.focalPointX / focalPointData.rawImageWidth) * 100);
   let y = Math.round((focalPointData.focalPointY / focalPointData.rawImageHeight) * 100);
 
-  // Some ugly maths to basically just sweetenen to percentage, resulting
-  // in a slightly higher value (* 1.1) around the 25% and 75% points
-  // to get the position close to what we actually want visually
+  // Some ugly maths to basically just sweetenen the percentage, resulting
+  // in a slightly higher value (up to * 1.1 around the 25% and 75% points)
+  // to get the position close to what we actually want visually with our
+  // fluid layout and 'cover' object-fit CSS rule
   x *= (0.1 / 25) * (25 - Math.abs((x % (2 * 25)) - 25)) + 1;
   y *= (0.1 / 25) * (25 - Math.abs((y % (2 * 25)) - 25)) + 1;
 
   // Return this directly as a percentage to be used within CSS;
-  // hard pixel values won't ever work, given the object-fit: contains
-  // rule and fluid layout
+  // hard pixel values won't ever work, given that fluid layout
   return {
     x: `${x}%`,
     y: `${y}%`
