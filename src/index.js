cb6d65a16ab6c3ebd23c9049bf8af74c70b4ff43
Paul Dolden <paul@dolden.co.uk>
2022-10-26 11:52:17 +0100

    fix linting in index.js

--- src/index.js
+++ src/index.js
@@ -1,77 +1,76 @@
-/* eslint-disable */
 /* Theme */
-export { default as crTheme } from "./theme/crTheme/theme";
-export { default as srTheme } from "./theme/srTheme/theme";
-export { default as ThemeProvider } from "./theme/ThemeProvider";
+export { default as crTheme } from './theme/crTheme/theme';
+export { default as srTheme } from './theme/srTheme/theme';
+export { default as ThemeProvider } from './theme/ThemeProvider';
 
 /* Utils */
-export { default as hideVisually } from "./theme/shared/hideVisually";
-export { default as zIndex } from "./theme/shared/zIndex";
-export { default as whiteListed } from "./utils/whiteListed";
-export { default as spacing } from "./theme/shared/spacing";
-export { default as breakpoint } from "./theme/shared/breakpoint";
-export { media, screen, container } from "./theme/shared/size";
+export { default as hideVisually } from './theme/shared/hideVisually';
+export { default as zIndex } from './theme/shared/zIndex';
+export { default as whiteListed } from './utils/whiteListed';
+export { default as spacing } from './theme/shared/spacing';
+export { default as breakpoint } from './theme/shared/breakpoint';
+export { media, screen, container } from './theme/shared/size';
 
 /* Atoms */
-export { default as Text } from "./components/Atoms/Text/Text";
-export { default as Logo } from "./components/Atoms/Logo/Logo";
-export { default as Picture } from "./components/Atoms/Picture/Picture";
-export { default as Link } from "./components/Atoms/Link/Link";
-export { default as Button } from "./components/Atoms/Button/Button";
-export { default as RadioButton } from "./components/Atoms/RadioButton/RadioButton";
-export { default as Checkbox } from "./components/Atoms/Checkbox/Checkbox";
-export { default as Input } from "./components/Atoms/Input/Input";
-export { default as Select } from "./components/Atoms/Select/Select";
-export { default as TextArea } from "./components/Atoms/TextArea/TextArea";
-export { default as RichText } from "./components/Atoms/RichText/RichText";
-export { default as Pagination } from "./components/Atoms/Pagination/Pagination";
-export { default as SocialIcons } from "./components/Atoms/SocialIcons/SocialIcons";
-export { default as TextInputWithDropdown } from "./components/Atoms/TextInputWithDropdown/TextInputWithDropdown";
-export { default as ErrorText } from "./components/Atoms/ErrorText/ErrorText";
-export { default as Label } from "./components/Atoms/Label/Label";
-export { default as ButtonWithStates } from "./components/Atoms/ButtonWithStates/ButtonWithStates";
+export { default as Text } from './components/Atoms/Text/Text';
+export { default as Logo } from './components/Atoms/Logo/Logo';
+export { default as Picture } from './components/Atoms/Picture/Picture';
+export { default as Link } from './components/Atoms/Link/Link';
+export { default as Button } from './components/Atoms/Button/Button';
+export { default as RadioButton } from './components/Atoms/RadioButton/RadioButton';
+export { default as Checkbox } from './components/Atoms/Checkbox/Checkbox';
+export { default as Input } from './components/Atoms/Input/Input';
+export { default as Select } from './components/Atoms/Select/Select';
+export { default as TextArea } from './components/Atoms/TextArea/TextArea';
+export { default as RichText } from './components/Atoms/RichText/RichText';
+export { default as Pagination } from './components/Atoms/Pagination/Pagination';
+export { default as SocialIcons } from './components/Atoms/SocialIcons/SocialIcons';
+export { default as TextInputWithDropdown } from './components/Atoms/TextInputWithDropdown/TextInputWithDropdown';
+export { default as ErrorText } from './components/Atoms/ErrorText/ErrorText';
+export { default as Label } from './components/Atoms/Label/Label';
+export { default as ButtonWithStates } from './components/Atoms/ButtonWithStates/ButtonWithStates';
 
 /* Molecules */
-export { default as HeroBanner } from "./components/Molecules/HeroBanner/HeroBanner";
-export { default as InfoBanner } from "./components/Molecules/InfoBanner/InfoBanner";
-export { default as SingleMessage } from "./components/Molecules/SingleMessage/SingleMessage";
-export { default as Card } from "./components/Molecules/Card/Card";
-export { default as CardDs } from "./components/Molecules/CardDs/CardDs";
-export { default as Box } from "./components/Molecules/Box/Box";
-export { default as ArticleTeaser } from "./components/Molecules/ArticleTeaser/ArticleTeaser";
-export { default as Header } from "./components/Organisms/Header/Header";
-export { default as Donate } from "./components/Organisms/Donate/Donate";
-export { default as DoubleCopy } from "./components/Molecules/DoubleCopy/DoubleCopy";
-export { default as PartnerLink } from "./components/Molecules/PartnerLink/PartnerLink";
-export { default as Footer } from "./components/Organisms/Footer/Footer";
-export { default as SearchResult } from "./components/Molecules/SearchResult/SearchResult";
-export { default as SearchInput } from "./components/Molecules/SearchInput/SearchInput";
-export { default as ShareButton } from "./components/Molecules/ShareButton/ShareButton";
-export { default as VideoBanner } from "./components/Molecules/VideoBanner/VideoBanner";
-export { default as HeaderEsuWithIcon } from "./components/Molecules/HeaderEsuWithIcon/HeaderEsuWithIcon";
-export { default as Icon } from "./components/Atoms/SocialIcons/Icon/Icon";
-export { default as Typeahead } from "./components/Molecules/Typeahead/Typeahead";
-export { default as SchoolLookup } from "./components/Molecules/SchoolLookup/SchoolLookup";
-export { default as SingleMessageDs } from "./components/Molecules/SingleMessageDS/SingleMessageDs";
-export { default as Promo } from "./components/Molecules/Promo/Promo";
-export { default as Accordion } from "./components/Molecules/Accordion/Accordion";
-export { default as Countdown } from "./components/Molecules/Countdown/Countdown";
-export { default as Banner } from "./components/Molecules/Banner/Banner";
-export { default as Chip } from "./components/Molecules/Chip/Chip";
-export { default as Descriptor } from "./components/Molecules/Descriptor/Descriptor";
-export { default as Lookup } from "./components/Molecules/Lookup/Lookup";
-export { default as SimpleSchoolLookup } from "./components/Molecules/SimpleSchoolLookup/SimpleSchoolLookup";
+export { default as HeroBanner } from './components/Molecules/HeroBanner/HeroBanner';
+export { default as InfoBanner } from './components/Molecules/InfoBanner/InfoBanner';
+export { default as SingleMessage } from './components/Molecules/SingleMessage/SingleMessage';
+export { default as Card } from './components/Molecules/Card/Card';
+export { default as CardDs } from './components/Molecules/CardDs/CardDs';
+export { default as Box } from './components/Molecules/Box/Box';
+export { default as ArticleTeaser } from './components/Molecules/ArticleTeaser/ArticleTeaser';
+export { default as Header } from './components/Organisms/Header/Header';
+export { default as Donate } from './components/Organisms/Donate/Donate';
+export { default as DoubleCopy } from './components/Molecules/DoubleCopy/DoubleCopy';
+export { default as PartnerLink } from './components/Molecules/PartnerLink/PartnerLink';
+export { default as Footer } from './components/Organisms/Footer/Footer';
+export { default as SearchResult } from './components/Molecules/SearchResult/SearchResult';
+export { default as SearchInput } from './components/Molecules/SearchInput/SearchInput';
+export { default as ShareButton } from './components/Molecules/ShareButton/ShareButton';
+export { default as VideoBanner } from './components/Molecules/VideoBanner/VideoBanner';
+export { default as HeaderEsuWithIcon } from './components/Molecules/HeaderEsuWithIcon/HeaderEsuWithIcon';
+export { default as Icon } from './components/Atoms/SocialIcons/Icon/Icon';
+export { default as Typeahead } from './components/Molecules/Typeahead/Typeahead';
+export { default as SchoolLookup } from './components/Molecules/SchoolLookup/SchoolLookup';
+export { default as SingleMessageDs } from './components/Molecules/SingleMessageDS/SingleMessageDs';
+export { default as Promo } from './components/Molecules/Promo/Promo';
+export { default as Accordion } from './components/Molecules/Accordion/Accordion';
+export { default as Countdown } from './components/Molecules/Countdown/Countdown';
+export { default as Banner } from './components/Molecules/Banner/Banner';
+export { default as Chip } from './components/Molecules/Chip/Chip';
+export { default as Descriptor } from './components/Molecules/Descriptor/Descriptor';
+export { default as Lookup } from './components/Molecules/Lookup/Lookup';
+export { default as SimpleSchoolLookup } from './components/Molecules/SimpleSchoolLookup/SimpleSchoolLookup';
 
 /* Organisms */
 export {
   EmailSignUp,
   buildEsuValidationSchema,
-  FIELDS as ESU_FIELDS,
-} from "./components/Organisms/EmailSignUp/_EmailSignUp";
-export { default as CookieBanner } from "./components/Organisms/CookieBanner/CookieBanner";
-export { default as Membership } from "./components/Organisms/Membership/Membership";
+  FIELDS as ESU_FIELDS
+} from './components/Organisms/EmailSignUp/_EmailSignUp';
+export { default as CookieBanner } from './components/Organisms/CookieBanner/CookieBanner';
+export { default as Membership } from './components/Organisms/Membership/Membership';
 export {
   MarketingPreferencesDS,
   setInitialValues,
-  buildValidationSchema,
-} from "./components/Organisms/MarketingPreferencesDS/_MarketingPreferencesDS";
+  buildValidationSchema
+} from './components/Organisms/MarketingPreferencesDS/_MarketingPreferencesDS';
