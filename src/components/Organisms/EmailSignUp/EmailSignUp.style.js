4b9a0ad076aefffc0734d6e7b7cf60e13adc62e0
Paul Dolden <paul@dolden.co.uk>
2022-10-31 11:31:59 +0000

    fix flex gap on column layout

--- src/components/Organisms/EmailSignUp/EmailSignUp.style.js
+++ src/components/Organisms/EmailSignUp/EmailSignUp.style.js
@@ -54,7 +54,9 @@ const NameWrapper = styled.div`
   @media ${({ theme }) => theme.breakpoint('medium')} {
     justify-content: start;
     flex-direction: ${({ columnLayout }) => (columnLayout ? 'column' : 'row')};
-    gap: ${spacing('md')};
+    gap: ${({ columnLayout }) => (columnLayout ? 0 : spacing('md'))};
+)}; 
+
   }
 `;
 
