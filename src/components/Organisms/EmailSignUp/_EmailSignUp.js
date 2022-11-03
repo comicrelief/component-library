47d4aabbb0ada7888d5d2de55d6d2b492870bb9a
Paul Dolden <paul@dolden.co.uk>
2022-11-03 15:36:24 +0000

    fix: make sure field errors are not displayed as formErrors

--- src/components/Organisms/EmailSignUp/_EmailSignUp.js
+++ src/components/Organisms/EmailSignUp/_EmailSignUp.js
@@ -102,12 +102,12 @@ const EmailSignUp = ({
         <>
           {/*
             Field errors will prevent submission,
-            so theoretically this should just be a single error set in the submission callback
-            with with RHF's `setError` method, but will neatly display multiple errors.
+            so theoretically the first error should set in the submission callback
+            with with RHF's `setError` method.
           */}
-          {Object.values(errors).map(error => (
-            <ErrorText>{error.message}</ErrorText>
-          ))}
+          {errors.formError !== undefined && (
+            <ErrorText>{errors.formError.message}</ErrorText>
+          )}
         </>
       )}
 
