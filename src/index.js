ea441ed1fcca1a34cf3ca560d143c5cf6742be30
Micheal O'Donovan <mikeodonovan3@gmail.com>
2022-11-25 17:41:00 +0000

    add missing main export

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ export { default as TextInputWithDropdown } from './components/Atoms/TextInputWi
 export { default as ErrorText } from './components/Atoms/ErrorText/ErrorText';
 export { default as Label } from './components/Atoms/Label/Label';
 export { default as ButtonWithStates } from './components/Atoms/ButtonWithStates/ButtonWithStates';
+export { default as Confetti } from './components/Atoms/Confetti/Confetti';
 
 /* Molecules */
 export { default as HeroBanner } from './components/Molecules/HeroBanner/HeroBanner';
