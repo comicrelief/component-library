9c68a74cb14dda1cd81cb6df14d56d9737b20e00
AndyEPhipps <a.phipps@comicrelief.com>
2022-12-01 12:47:43 +0000

    Fix typo

--- src/utils/Membership.js
+++ src/utils/Membership.js
@@ -58,7 +58,7 @@ const handleDonateSubmission = (
   }
   const givingTypeUrl = givingType === 'single' ? 'single' : 'monthly';
 
-  window.location.href = `${donateLink}?clientOverride=${clientID}&amount=${amount}&currency=GBP&givingType=${givingTypeUrl}&cartId=${cartID}&affiliate=${affiliateValue}&siteurl=${currentpageUrl}&rowID=${mbshipID}&popUpOpen=${popOpen}&&popUpShown=${popUpShown}`;
+  window.location.href = `${donateLink}?clientOverride=${clientID}&amount=${amount}&currency=GBP&givingType=${givingTypeUrl}&cartId=${cartID}&affiliate=${affiliateValue}&siteurl=${currentpageUrl}&rowID=${mbshipID}&popUpOpen=${popOpen}&popUpShown=${popUpShown}`;
 };
 
 export {
