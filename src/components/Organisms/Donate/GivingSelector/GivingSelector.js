37378d90df19b756d839b1a36f909970c3b7b1d6
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-04-28 16:40:31 +0100

    change onClick to onChange in GivingSelector

--- src/components/Organisms/Donate/GivingSelector/GivingSelector.js
+++ src/components/Organisms/Donate/GivingSelector/GivingSelector.js
@@ -28,7 +28,7 @@ const GivingSelector = ({
           label=""
           errormsg=""
           checked={givingType === 'single'}
-          onClick={() => handleGivingTypeChange('single', givingType)}
+          onChange={() => handleGivingTypeChange('single', givingType)}
         />
         <Label active={givingType === 'single'} htmlFor={`give-once--${mbshipID}`}>
           Single
@@ -42,7 +42,7 @@ const GivingSelector = ({
           label=""
           errormsg=""
           checked={givingType === 'monthly'}
-          onClick={() => handleGivingTypeChange('monthly', givingType)}
+          onChange={() => handleGivingTypeChange('monthly', givingType)}
         />
         <Label active={givingType === 'monthly'} htmlFor={`give-monthly--${mbshipID}`}>
           Monthly
