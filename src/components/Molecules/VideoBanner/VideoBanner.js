bf2b14d0293e2236e96ad72e3783ef201c6e5915
AndyEPhipps <a.phipps@comicrelief.com>
2023-08-03 16:03:05 +0100

    Add in more options

--- src/components/Molecules/VideoBanner/VideoBanner.js
+++ src/components/Molecules/VideoBanner/VideoBanner.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Video = styled.video.attrs(() => ({
-  playsInline: true,
-  muted: true,
-  loop: false
+  playsInline: true
 }))`
   width: 100%;
   height: 100%;
 `;
 
 const VideoBanner = ({
-  video, poster, showUserControls, autoPlay
+  video, poster, showUserControls, autoPlay, loop, muted, showPosterAfterPlaying
 }) => {
   const videoEl = useRef(null);
 
@@ -21,30 +19,50 @@ const VideoBanner = ({
   };
 
   useEffect(() => {
+    // Only trigger play where appropriate:
     if (autoPlay) {
       onPlay();
-    } else {
-      console.log('No autoplay');
+    }
+
+    // Only attach event listener where appropriate:
+    if (!loop && showPosterAfterPlaying) {
+      videoEl.current.addEventListener('ended', () => {
+        // Reloads video, showing poster
+        videoEl.current.load();
+      });
     }
   });
 
   return (
-    <Video poster={poster} src={video} ref={videoEl} controls={showUserControls}>
+    <Video
+      poster={poster}
+      src={video}
+      ref={videoEl}
+      controls={showUserControls}
+      loop={loop}
+      muted={muted}
+    >
       Your browser does not support video.
     </Video>
   );
 };
 
 VideoBanner.defaultProps = {
-  showUserControls: true, // CHANGE
-  autoPlay: false // CHANGE
+  showUserControls: true,
+  autoPlay: false,
+  loop: false,
+  muted: true,
+  showPosterAfterPlaying: true
 };
 
 VideoBanner.propTypes = {
   video: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   showUserControls: PropTypes.bool,
-  autoPlay: PropTypes.bool
+  autoPlay: PropTypes.bool,
+  loop: PropTypes.bool,
+  muted: PropTypes.bool,
+  showPosterAfterPlaying: PropTypes.bool
 };
 
 export default VideoBanner;
