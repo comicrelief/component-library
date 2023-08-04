725bee40e3ac1214d07946fa24f08130b0b1e83b
Andy E Phipps <a.phipps@comicrelief.com>
2023-08-04 12:16:43 +0100

    feat: Video Banner Updates (#579)
    * Use props rather than hardcoded attributes
    
    * Add in more options
    
    * Test update
    
    * feat: Video Banner updates
    
    * Clarify comments and func name
    
    * Add match aspect ratio poster img
    
    * Rename prop
    
    * Update tests with new dynamic class name

--- src/components/Molecules/VideoBanner/VideoBanner.js
+++ src/components/Molecules/VideoBanner/VideoBanner.js
@@ -3,36 +3,66 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Video = styled.video.attrs(() => ({
-  autoPlay: true,
-  playsInline: true,
-  muted: true,
-  loop: false
+  playsInline: true
 }))`
   width: 100%;
   height: 100%;
 `;
 
-const VideoBanner = ({ video, poster }) => {
+const VideoBanner = ({
+  video, poster, controls, autoPlay, loop, muted, showPosterAfterPlaying
+}) => {
   const videoEl = useRef(null);
 
-  const onPlay = () => {
+  const triggerPlay = () => {
     videoEl.current.play();
   };
 
   useEffect(() => {
-    onPlay();
+    // Trigger onload autoplay based on prop:
+    if (autoPlay) {
+      triggerPlay();
+    }
+
+    // And attach event listener based on prop:
+    if (!loop && showPosterAfterPlaying) {
+      videoEl.current.addEventListener('ended', () => {
+        // Reloads video, which re-shows poster
+        videoEl.current.load();
+      });
+    }
   });
 
   return (
-    <Video poster={poster} src={video} ref={videoEl}>
+    <Video
+      poster={poster}
+      src={video}
+      ref={videoEl}
+      controls={controls}
+      loop={loop}
+      muted={muted}
+    >
       Your browser does not support video.
     </Video>
   );
 };
 
+VideoBanner.defaultProps = {
+  showPosterAfterPlaying: true,
+  controls: false,
+  autoPlay: true,
+  loop: false,
+  muted: true
+};
+
 VideoBanner.propTypes = {
+  showPosterAfterPlaying: PropTypes.bool,
   video: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  controls: PropTypes.bool,
+  autoPlay: PropTypes.bool,
+  loop: PropTypes.bool,
+  muted: PropTypes.bool
 };
 
 export default VideoBanner;
