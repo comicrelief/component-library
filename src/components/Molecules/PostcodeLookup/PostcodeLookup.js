db844171620c5eb36a32f7e065d4e15f4f28a341
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-08-09 16:27:47 +0100

    Most functionality imported

--- src/components/Molecules/PostcodeLookup/PostcodeLookup.js
+++ src/components/Molecules/PostcodeLookup/PostcodeLookup.js
@@ -1,9 +1,69 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { isValid } from 'postcode';
+import axios from 'axios';
+import Lookup from '../Lookup/Lookup';
 
-const PostcodeLookup = () => <h1>PostcodeLookup</h1>;
+const validatePostcode = postcode => {
+  const trimmed = typeof postcode === 'string' ? postcode.trim() : '';
+  return isValid(trimmed) && postcode.toNormalised(trimmed);
+};
+
+const getAddresses = postcode => axios.get(
+  `${process.env.GATSBY_LOOKUP_SERVICE_BASE_URL}/postcode/lookup`,
+  {
+    timeout: 10000,
+    params: { query: postcode }
+  }
+).then(res => res.data.addresses || []);
+
+const addressToString = address => [address.Line1, address.Line2, address.Line3, address.posttown]
+  .filter(line => line)
+  .join(', ');
+
+const addressFetcher = async postcode => {
+  const valid = validatePostcode(postcode);
+  if (!valid) {
+    throw new Error('Please provide a valid postcode');
+  }
+  try {
+    return await getAddresses(valid);
+  } catch (error) {
+    // eslint-disable-next-line
+    if (typeof Sentry !== 'undefined') {
+      // eslint-disable-next-line
+      Sentry.captureException(error);
+    }
+    throw new Error('Sorry, something unexpected went wrong. Please try again or enter your address manually');
+  }
+};
+
+/**
+ * A simple postcode lookup component
+ *
+ * The parent component must provide an `onSelect` prop in order to receive the selected address.
+ *
+ * @param onSelect
+ * @param rest
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const PostcodeLookup = ({ onSelect, ...rest }) => (
+  <Lookup
+    name="postcode_lookup"
+    label="Find address by postcode"
+    placeholder="Enter postcode..."
+    buttonText="Find address"
+    noResultsMessage="Sorry, could not find any addresses for that postcode"
+    mapOptionToString={addressToString}
+    lookupHandler={addressFetcher}
+    onSelect={onSelect}
+    {...rest}
+  />
+);
 
 PostcodeLookup.propTypes = {
+  onSelect: PropTypes.func.isRequired
 };
 
 export default PostcodeLookup;
