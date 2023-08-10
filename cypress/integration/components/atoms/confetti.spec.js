cd1d5eae55cbe3af44dbd654d874b08afa152922
Krupa Pammi <krupa.pammi@gmail.com>
2023-08-10 16:08:03 +0100

    test: tidy up and add tests for additional components (#583)
    * test: tidy up and add tests for additional components
    
    * test: add video banner test
    
    * test: fix video banner test

---
+++ cypress/integration/components/atoms/confetti.spec.js
@@ -0,0 +1,52 @@
+describe('Confetti component', () => {
+  before(() => {
+    // go to confetti component
+    cy.visit('/#confetti');
+  });
+
+  describe('props and methods section', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="Confetti-container"]')
+        .as('container')
+        .find('[class^=rsg--preview]')
+        .as('preview');
+
+      cy.get('@container')
+        .find('button')
+        .contains('View Code')
+        .as('viewCodeBtn');
+    });
+
+    it('renders button with states', () => {
+      cy.get('[data-testid="Confetti-examples"]')
+        .find('h1')
+        .contains('Confetti')
+        .should('exist');
+    });
+
+    it('renders confetti when "trigger confetti" is clicked', () => {
+      //verify button as a span
+      cy.get('[data-testid="Confetti-example-1"]')
+        .contains('trigger confetti')
+        .click()
+    });
+
+    it('has view code button', () => {
+      cy.get('@viewCodeBtn').should('exist');
+    });
+
+    it('shows code on click', () => {
+      cy.get('@viewCodeBtn').click();
+      cy.get('@container')
+        .find('textarea')
+        .should('exist');
+    });
+
+    it('closes code on click', () => {
+      cy.get('@viewCodeBtn').click();
+      cy.get('@container')
+        .find('textarea')
+        .should('not.exist');
+    });
+  });
+});
---
+++ cypress/integration/components/molecules/accordion.spec.js
@@ -0,0 +1,72 @@
+describe('Accordion component', () => {
+  before(() => {
+    // go to accordion component
+    cy.visit('/#accordion');
+  });
+
+  describe('props and methods section', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="Accordion-container"] button[name="rsg-usage"]')
+        .contains('Props & methods')
+        .as('propsBtn');
+
+      cy.get('@propsBtn')
+        .closest('[class^=rsg--tabs]')
+        .as('container');
+    });
+
+    it('is present', () => {
+      cy.get('@propsBtn').should('exist');
+    });
+
+    it('does not show table initially', () => {
+      cy.get('@container')
+        .find('table')
+        .should('not.exist');
+    });
+
+    it('shows the table on button click', () => {
+      cy.get('@propsBtn').click();
+      cy.get('@container')
+        .find('table')
+        .should('contain', 'Prop name');
+    });
+  });
+
+  describe('Accordion section', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="Accordion-example-1"]')
+        .as('container')
+        .find('[data-preview="Accordion"]')
+        .as('preview');
+
+      cy.get('@container')
+        .find('button')
+        .contains('View Code')
+        .as('viewCodeBtn');
+    });
+
+    it('clicking svg dropdown icon should element p', () => {
+      cy.get('[data-testid="Accordion-example-1"] button > div').click();
+      cy.get('p').should('exist');
+    });
+
+    it('has view code button', () => {
+      cy.get('@viewCodeBtn').should('exist');
+    });
+
+    it('shows code on click', () => {
+      cy.get('@viewCodeBtn').click();
+      cy.get('@container')
+        .find('textarea')
+        .should('exist');
+    });
+
+    it('closes code on click', () => {
+      cy.get('@viewCodeBtn').click();
+      cy.get('@container')
+        .find('textarea')
+        .should('not.exist');
+    });
+  });
+});
