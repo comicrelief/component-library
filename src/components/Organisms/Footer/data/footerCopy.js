11a0a5c56e21986d0974d4bcd71caf9ef9ee27b8
Andy E Phipps <a.phipps@comicrelief.com>
2023-08-16 10:55:26 +0100

    fix: update footer address (#584)

--- src/components/Organisms/Footer/data/footerCopy.js
+++ src/components/Organisms/Footer/data/footerCopy.js
@@ -1,4 +1,4 @@
 export default {
   copy:
-    'Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). Registered address: 1st Floor, 89 Albert Embankment, London, SE1 7TP.'
+    'Comic Relief is the trading name of Charity Projects, a registered charity in England and Wales (326568) and Scotland (SC039730), which is a company limited by guarantee registered in England and Wales (01806414). Registered address: 6th Floor, The White Chapel Building, 10 Whitechapel High Street, London, E1 8QS.'
 };
