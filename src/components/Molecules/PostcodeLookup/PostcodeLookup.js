626aaf76400c39f0e25a8a70d8010faab4c1fff5
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-10-02 13:38:15 +0100

    bring in FSU code

--- src/components/Molecules/PostcodeLookup/PostcodeLookup.js
+++ src/components/Molecules/PostcodeLookup/PostcodeLookup.js
@@ -1,61 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isValid, toNormalised } from 'postcode';
 import axios from 'axios';
+import Lookup from '../Lookup/Lookup';
 
-import Typeahead from '../Typeahead/Typeahead';
-
-const optionFetcher = async query => {
-  const response = await axios.get(
-    'https://lookups.sls.comicrelief.com/schools/lookup',
-    {
-      params: { query },
-      timeout: 3000
-    }
-  );
-  return response.data.data.schools;
+const validatePostcode = postcode => {
+  const trimmed = typeof postcode === 'string' ? postcode.trim() : '';
+  return isValid(trimmed) && toNormalised(trimmed);
 };
 
-const optionParser = school => `${school.name}, ${school.post_code}`;
-
-const PostcodeLookup = React.forwardRef(
-  (
-    {
-      label,
-      placeholder,
-      notFoundMessage,
-      onSelect,
-      ...rest
-    },
-    ref
-  ) => {
-    const props = {
-      optionFetcher,
-      optionParser,
-      onSelect,
-      id: 'school-lookup',
-      name: 'school-lookup',
-      label,
-      placeholder,
-      notFoundMessage,
-      ...rest
-    };
-
-    return <Typeahead {...props} ref={ref} />;
+const getAddresses = postcode => axios.get(
+  `${process.env.GATSBY_LOOKUP_SERVICE_BASE_URL}/postcode/lookup`,
+  {
+    timeout: 10000,
+    params: { query: postcode }
   }
+).then(res => res.data.addresses || []);
+
+const addressToString = address => [address.Line1, address.Line2, address.Line3, address.posttown]
+  .filter(line => line)
+  .join(', ');
+
+const addressFetcher = async postcode => {
+  const valid = validatePostcode(postcode);
+  if (!valid) {
+    throw new Error('Please provide a valid postcode');
+  }
+  try {
+    return await getAddresses(valid);
+  } catch (error) {
+    // if (typeof Sentry !== 'undefined') {
+    //   Sentry.captureException(error);
+    // }
+    throw new Error('Sorry, something unexpected went wrong. Please try again or enter your address manually');
+  }
+};
+
+/**
+ * A simple postcode lookup component
+ *
+ * The parent component must provide an `onSelect` prop in order to receive the selected address.
+ *
+ * @param onSelect
+ * @param rest
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const PostcodeLookup = ({ onSelect, ...rest }) => (
+  <Lookup
+    name="postcode_lookup"
+    label="Find address by postcode"
+    placeholder="Enter postcode..."
+    buttonText="Find address"
+    noResultsMessage="Sorry, could not find any addresses for that postcode"
+    mapOptionToString={addressToString}
+    lookupHandler={addressFetcher}
+    onSelect={onSelect}
+    {...rest}
+  />
 );
 
 PostcodeLookup.propTypes = {
-  /** This function is used to provide data to the parent component when a selection is made. */
-  onSelect: PropTypes.func.isRequired,
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
-  notFoundMessage: PropTypes.string
-};
-
-PostcodeLookup.defaultProps = {
-  label: 'Find address by postcode',
-  placeholder: 'Enter postcode...',
-  notFoundMessage: "Sorry, we can't find this address"
+  onSelect: PropTypes.func.isRequired
 };
 
 export default PostcodeLookup;
