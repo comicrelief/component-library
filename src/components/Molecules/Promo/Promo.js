0d4089d2c66f3bdea3e13bc43e793e4f6afc393c
AndyEPhipps <a.phipps@comicrelief.com>
2023-10-05 15:57:32 +0100

    Shift promo video stuff over into own component

--- src/components/Molecules/Promo/Promo.js
+++ src/components/Molecules/Promo/Promo.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import PromoVideoButton from './_PromoVideoButton';
-import Picture from '../../Atoms/Picture/Picture';
+import PromoVideo from './_PromoVideo';
 import { sizes } from '../../../theme/shared/breakpoint';
 
+import Picture from '../../Atoms/Picture/Picture';
+// import { sizes } from '../../../theme/shared/breakpoint';
+
 import {
-  Container, Wrapper, Copy, Media, Video
+  Container, Wrapper, Copy, Media
 } from './Promo.style';
 
 const Promo = ({
@@ -26,92 +28,40 @@ const Promo = ({
   mobileVideoSrc,
   lightVideo
 }) => {
-  // To be updated via useEffect on load:
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isRestarting, setIsRestarting] = useState(false);
   // Store the appropriate prop in state, dependent on the breakpoint
   const [thisVideoSrc, setThisVideoSrc] = useState(null);
   const [thisPoster, setThisPoster] = useState(null);
-
-  const [videoProgress, setVideoProgress] = useState(0);
-  const videoEl = useRef(null);
-
-  const togglePlay = () => {
-    if (isPlaying) videoEl.current.pause();
-    else videoEl.current.play();
-    setIsPlaying(!isPlaying);
-  };
-
   const hasVideo = (video || mobileVideoSrc) || false;
   // Video Promo will override and ignore any 'non-Video' images
   const hasImage = (imageSet && !hasVideo) || false;
-
-  const updateTime = () => {
-    if (videoEl.current.duration) {
-      // Calculate the percentage of the video played:
-      const percentage = Math.round((videoEl.current.currentTime / videoEl.current.duration) * 100);
-      const nearest = 25; // As a means of smoothing the data a bit, still playing around with it
-      const roundedPercentage = (percentage + nearest / 2) - ((percentage + nearest / 2) % nearest);
-      setVideoProgress(roundedPercentage);
-    }
-  };
 
   // Runs on initial load:
   useEffect(() => {
     if (hasVideo) {
       // Checks size on load ONLY; don't want to mess about with listeners:
       const isDesktop = window.innerWidth >= sizes.nav; // 1024+ px
+      let currentPoster; let currentSrc;
 
       // If we've got both desktop AND mobile videos, let
       // the breakpoint define which video src to use:
       if (video && mobileVideoSrc) {
-        setThisVideoSrc(isDesktop ? video : mobileVideoSrc);
-        setThisPoster(isDesktop ? poster : mobilePoster);
+        currentSrc = isDesktop ? video : mobileVideoSrc;
+        currentPoster = isDesktop ? poster : mobilePoster;
       } else {
-        // Else, pick whatever we do actually have
-        setThisVideoSrc(video || mobileVideoSrc);
-        setThisPoster(poster || mobilePoster);
-      }
-    }
-  }, [hasVideo, video, mobileVideoSrc]);
-
-  // Only loads once the initial screensize check is complete
-  useEffect(() => {
-    if (hasVideo && thisVideoSrc !== null) {
-      videoEl.current.addEventListener('timeupdate', updateTime);
-      // Trigger on-load autoplay if apppropriate
-      if (autoPlay && hasVideo && !isPlaying) {
-        togglePlay();
+        // Else, pick whatever we DO actually have:
+        currentSrc = video || mobileVideoSrc;
+        currentPoster = poster || mobilePoster;
       }
 
-      videoEl.current.addEventListener('ended', () => {
-        // Used purely to halt the CSS animation:
-        setIsRestarting(true);
-
-        // If this is a non-looping video, add a listener to update our local state
-        // once the video's ended, to let the user retrigger it manually:
-        if (!loop) {
-          setIsPlaying(false);
-          setVideoProgress(0);
-          // Reload the video to show the poster image:
-          if (showPosterAfterPlaying) videoEl.current.load();
-        } else {
-          // Rather than using the Video 'loop' property, we retrigger
-          // it in *code* as there's no 'restarted' to hook into:
-          togglePlay();
-        }
-
-        // Grace period to allow the animation to reset
-        setTimeout(() => { setIsRestarting(false); }, 100);
-      });
+      setThisVideoSrc(currentSrc);
+      setThisPoster(currentPoster);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [thisVideoSrc]);
+  }, [hasVideo, video, mobileVideoSrc, mobilePoster, poster]);
 
   return (
     <Container backgroundColor={backgroundColor} position={position}>
-      {(hasImage && !hasVideo) && (
       <Media imageRight={copyLeft}>
+        {(hasImage && !hasVideo) && (
         <Picture
           alt={imageAltText}
           imageLow={imageLow}
@@ -121,28 +71,46 @@ const Promo = ({
           width="100%"
           height="100%"
         />
+        )}
+        {(hasVideo && thisVideoSrc) && (
+        <>
+          <PromoVideo
+            copyLeft={copyLeft}
+            lightVideo={lightVideo}
+            thisVideoSrc={thisVideoSrc}
+            thisPoster={thisPoster}
+            showPosterAfterPlaying={showPosterAfterPlaying}
+            autoPlay={autoPlay}
+            loop={loop}
+            // ref={videoEl}
+            // isRestarting={isRestarting}
+            // thisPoster={thisPoster}
+            // videoProgress={videoProgress}
+            // thisVideoSrc={thisVideoSrc}
+            // lightVideo={lightVideo}
+            // isPlaying={isPlaying}
+            // togglePlay={togglePlay}
+          />
+          {/* <Video
+            ref={videoEl}
+            src={thisVideoSrc}
+            poster={thisPoster}
+            muted
+          >
+            Your browser does not support video.
+          </Video>
+          <PromoVideoButton
+            copyLeft={copyLeft}
+            videoProgress={videoProgress}
+            togglePlay={togglePlay}
+            isPlaying={isPlaying}
+            lightVideo={lightVideo}
+            isRestarting={isRestarting}
+          /> */}
+        </>
+        )}
       </Media>
-      )}
-      {(hasVideo && thisVideoSrc) && (
-      <Media imageRight={copyLeft}>
-        <Video
-          ref={videoEl}
-          src={thisVideoSrc}
-          poster={thisPoster}
-          muted
-        >
-          Your browser does not support video.
-        </Video>
-        <PromoVideoButton
-          copyLeft={copyLeft}
-          videoProgress={videoProgress}
-          togglePlay={togglePlay}
-          isPlaying={isPlaying}
-          lightVideo={lightVideo}
-          isRestarting={isRestarting}
-        />
-      </Media>
-      )}
+
       <Wrapper copyLeft={copyLeft}>
         <Copy position={position} hasVideo={hasVideo} lightVideo={lightVideo}>
           {children}
