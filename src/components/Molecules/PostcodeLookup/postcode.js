6868bf5931c01fcaf75dd4eca59a902e257e935a
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-10-12 17:16:01 +0100

    works with right postcode data : )

--- src/components/Molecules/PostcodeLookup/postcode.js
+++ src/components/Molecules/PostcodeLookup/postcode.js
@@ -1,8 +1,7 @@
 // https://jsonplaceholder.typicode.com/albums/2
 import axios from 'axios';
 
-const endpoint = 'https://jsonplaceholder.typicode.com/albums/2';
-const endpoint2 = 'https://lookups-staging.sls.comicrelief.com/postcode/lookup/bs56hq';
+const endpoint = 'https://lookups-staging.sls.comicrelief.com/postcode/lookup/bs56hq';
 
 export const fetchPostcode = function () {
   return axios
