7d9712ef0572d827adbfd23c8fc14d5ddff77afb
AndyEPhipps <a.phipps@comicrelief.com>
2023-10-26 13:26:48 +0100

    Tidyup

--- src/components/Organisms/ImpactSlider/_utils.js
+++ src/components/Organisms/ImpactSlider/_utils.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const getUrlParameter = name => {
   const text = name.replace(/\[]/, '\\[').replace(/[\]]/, '\\]');
   const regex = new RegExp(`[\\?&]${text}=([^&#]*)`);
@@ -9,31 +8,22 @@ const getUrlParameter = name => {
   return param;
 };
 
-const handleSubmission = (
-  amount,
-  donateLink,
-  clientID,
-  rowID
-) => {
+const handleSubmission = (amount, donateLink, clientID, rowID) => {
   let currentpageUrl = window.location.href;
 
-  // to-do: do we need this?
   const affiliateValue = getUrlParameter(currentpageUrl);
 
   const reg = /[#?|[\]\\]/g;
 
   if (currentpageUrl.match(reg) !== null) {
     const thisMatch = currentpageUrl.match(reg)[0];
-    currentpageUrl = currentpageUrl.substring(
-      0,
-      currentpageUrl.indexOf(thisMatch)
-    );
+    currentpageUrl = currentpageUrl.substring(0, currentpageUrl.indexOf(thisMatch));
   }
 
-  // TO-DO:
-  // - do we need client IDs?
-  // - do we need affliate?
+  // - Q: do we need client IDs? A: 99% of examples just use 'donate' (or 'test' for staging)
+  //
   // - I guess we still need rowID for the back-browsing scroll stuff?
+  // - TO-DO: add in said scroll stuff
   const forwardingUrl = `${donateLink}?amount=${amount}&clientOverride=${clientID}&currency=GBP&givingType=single&affiliate=${affiliateValue}&siteurl=${currentpageUrl}&rowID=${rowID}`;
 
   console.log(JSON.stringify(forwardingUrl));
