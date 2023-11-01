65d2724f2c728c1517e75cd9e9dee77b5796665b
AndyEPhipps <a.phipps@comicrelief.com>
2023-11-01 16:21:37 +0000

    Remove impossible drag-drop test stuff

--- cypress/e2e/components/organisms/impactSlider.cy.js
+++ cypress/e2e/components/organisms/impactSlider.cy.js
@@ -5,44 +5,41 @@ describe('Impact Slider component', () => {
     cy.visit('/#impactslider');
   });
 
-  //   describe('props and methods section', () => {
-  //     beforeEach(() => {
-  //       cy.visit('/#impactslider');
-  //       cy.get('[data-testid="ImpactSlider-container"] button[name="rsg-usage"]')
-  //         .contains('Props & methods')
-  //         .as('propsBtn');
+  describe('props and methods section', () => {
+    beforeEach(() => {
+      cy.visit('/#impactslider');
+      cy.get('[data-testid="ImpactSlider-container"] button[name="rsg-usage"]')
+        .contains('Props & methods')
+        .as('propsBtn');
 
-  //       cy.get('@propsBtn')
-  //         .closest('[class^=rsg--tabs]')
-  //         .as('container');
-  //     });
+      cy.get('@propsBtn')
+        .closest('[class^=rsg--tabs]')
+        .as('container');
+    });
 
-  //     it('is present', () => {
-  //       cy.get('@propsBtn').should('exist');
-  //     });
+    it('is present', () => {
+      cy.get('@propsBtn').should('exist');
+    });
 
-  //     it('does not show table initially', () => {
-  //       cy.get('@container')
-  //         .find('table')
-  //         .should('not.exist');
-  //     });
+    it('does not show table initially', () => {
+      cy.get('@container')
+        .find('table')
+        .should('not.exist');
+    });
 
-  //     it('shows the table on button click', () => {
-  //       cy.get('@propsBtn').click();
-  //       cy.get('@container')
-  //         .find('table')
-  //         .should('contain', 'Prop name');
-  //     });
-  //   });
+    it('shows the table on button click', () => {
+      cy.get('@propsBtn').click();
+      cy.get('@container')
+        .find('table')
+        .should('contain', 'Prop name');
+    });
+  });
 
   describe('Check test values and interaction', () => {
     beforeEach(() => {
       cy.visit('/#impactslider');
       cy.get('[data-testid="ImpactSlider-example-1"]')
         .as('sliderOne');
-
-      cy.get('[data-testid="ImpactSlider-example-3"]')
-        .as('sliderTwo');
 
       cy.get('@sliderOne')
         .find('[data-testid="impact-slider--moneybuy-1"]')
@@ -63,10 +60,6 @@ describe('Impact Slider component', () => {
       cy.get('@sliderOne')
         .find('.range-slider__thumb[data-upper="true"]')
         .as('sliderThumb');
-
-      cy.get('@sliderTwo')
-        .find('.range-slider__thumb[data-upper="true"]')
-        .as('sliderThumbTwo');
     });
 
     it('All moneybuys are present with default values', () => {
@@ -74,42 +67,6 @@ describe('Impact Slider component', () => {
       cy.get('@moneybuy2').should('exist').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
       cy.get('@moneybuy3').should('exist').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
       cy.get('@moneybuy4').should('exist').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
-    });
-
-    it('Slider should change values', () => {
-      cy.get('@sliderThumb').should('exist').then($el => {
-        const initialPosition = $el[0].getBoundingClientRect();
-        console.log('initialPosition', initialPosition);
-        const initialX = Math.floor(initialPosition.x);
-        const initialY = Math.floor(initialPosition.y);
-        const newX = initialX + 200;
-        console.log('THUMB1 Initial position:', initialX, initialY);
-        cy.get('@sliderThumb').should('exist')
-          .trigger('mousedown', { which: 1, pageX: initialX, pageY: initialY })
-          .trigger('mousemove', { which: 1, pageX: newX, pageY: initialY })
-          .trigger('mouseup');
-        cy.get('@sliderThumb').should('exist');
-      });
-
-      // cy.get('@sliderThumbTwo').should('exist').then($el => {
-      //   const initialPosition = $el[0].getBoundingClientRect();
-      //   console.log('initialPosition', initialPosition);
-      //   const initialX = initialPosition.x;
-      //   const initialY = initialPosition.y;
-      //   const newX = initialX + 200;
-      //   console.log('THUMB2 Initial position:', initialX, initialY);
-      //   cy.get('@sliderThumb').should('exist')
-      //   .trigger('mousedown', { which: 1, pageX: initialX, pageY: initialY })
-      //     .trigger('mousemove', { which: 1, pageX: newX, pageY: initialY })
-      //     .trigger('mouseup');
-      //   cy.get('@sliderThumb').should('exist');
-      // });
-
-      // console.log('found X!', x);
-      // cy.get('@moneybuy1').children('[data-testid="moneybuy-amount"]').should('have.text', '5');
-      // cy.get('@moneybuy2').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
-      // cy.get('@moneybuy3').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
-      // cy.get('@moneybuy4').children('[data-testid="moneybuy-amount"]').should('have.text', '0');
     });
   });
 });
