7e61e1bce5bc136703212645b2e473dde03e2744
Micheal O'Donovan <mikeodonovan3@gmail.com>
2024-04-16 10:41:44 +0100

    clean up noise 2

--- src/components/Molecules/PartnerLink/PartnerLink.test.js
+++ src/components/Molecules/PartnerLink/PartnerLink.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "jest-styled-components";
+import React from 'react';
+import 'jest-styled-components';
 
-import renderWithTheme from "../../../hoc/shallowWithTheme";
-import PartnerLink from "./PartnerLink";
-import Picture from "../../Atoms/Picture/Picture";
-import { defaultData } from "../../../styleguide/data/data";
-it("renders correctly", () => {
+import renderWithTheme from '../../../hoc/shallowWithTheme';
+import PartnerLink from './PartnerLink';
+import Picture from '../../Atoms/Picture/Picture';
+import { defaultData } from '../../../styleguide/data/data';
+it('renders correctly', () => {
   const tree = renderWithTheme(
     <PartnerLink link="https://www.comicrelief.com">
       <Picture
