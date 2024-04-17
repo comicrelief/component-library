a70cc9dd7398a9ea4362c62a458133ea1c3ac56e
Micheal O'Donovan <76488110+michael-odonovan@users.noreply.github.com>
2024-04-17 15:40:12 +0100

    feat: creates extra Cards options / functionality. (#637)
    * basics of row functionality working
    
    * lintfix
    
    * snaps
    
    * add test
    
    * adjust how props trigger layout
    
    * adjust spacing
    
    * refactor
    
    * add comments
    
    * add trial breakpoints
    
    * Empty commit to trigger build
    
    * adjust names
    
    * snaps
    
    * add corners on image
    
    * snaps
    
    * adjust logic for rounded corners
    
    * square pic for Row view
    
    * snaps and lint
    
    * adjust comments
    
    * change new breakpoints file to allBreakpoints
    
    * start using allBreakpoints in Cards work
    
    * snaps
    
    * change props to boolean
    
    * refactor prop
    
    * break out styles from Card
    
    * lintfix
    
    * update
    
    * snaps
    
    * inherit fix
    
    * before remove Picture styles
    
    * remove Picture styles to troubleshoot
    
    * snaps
    
    * back to less
    
    * Copy small breakpoint all OK
    
    * Copy tablet view all good
    
    * snaps
    
    * Picture 1st stage fix snaps
    
    lintfix
    
    * clean up noise 1
    
    * clean up noise 2
    
    * back to first stage
    
    * incorporate new allBreakpoints work
    
    * try to remove empty @media
    
    * update
    
    * snaps
    
    * update
    
    * media breakpoint inside
    
    * Picture M break fix
    
    * lintfix
    
    * redo Cards
    
    * snaps
    
    * adjust comments
    
    * set styles back to global.css values
    
    * redo cards small layout
    
    * Cards style both S and M adjusted
    
    * review fixes
    
    * review edits and bug fix

--- src/components/Molecules/Card/Card.test.js
+++ src/components/Molecules/Card/Card.test.js
@@ -14,6 +14,23 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
+it('renders correctly', () => {
+  const tree = renderWithTheme(
+    <Card
+      image={defaultData.image}
+      images={defaultData.image}
+      backgroundColor="yellow"
+      smallBreakpointLayout="Row"
+      mediumBreakpointLayout="Row"
+    >
+      <h2>Title</h2>
+      <p>Text body copy description. Loads more text test. Loads more text test.</p>
+    </Card>
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
 it('renders correctly with no body', () => {
   const tree = renderWithTheme(
     <Card imageLow={defaultData.image} images={defaultData.image} backgroundColor="yellow" />
