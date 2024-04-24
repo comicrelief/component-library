f0c83e9323878feeb26056f6c53d12bc537acb84
AndyEPhipps <a.phipps@comicrelief.com>
2024-04-24 18:05:28 +0100

    Fix shop selector with new markup

--- playwright/components/organisms/header.spec.js
+++ playwright/components/organisms/header.spec.js
@@ -60,7 +60,7 @@ test.describe('header component', () => {
     // CR logo
     await expect(page.locator('[data-testid="Header-example-9"] [title="Go to Comic Relief homepage"]')).toBeVisible();
     // shop icon
-    await expect(page.locator('[data-testid="Header-example-9"] header[role="banner"][title="Shop"]')).toBeVisible();
+    await expect(page.locator('[data-testid="Header-example-9"] header[role="banner"] [data-testid="meta-icons--desktop"] [title="Shop"]')).toBeVisible();
     // search
     await expect(page.locator('[data-testid="Header-example-9"] header[role="banner"] a[href="/search"]')).toBeVisible();
 
