61aa53b95368aad9b5515cd96520d456e6c26f10
Micheal O'Donovan <mikeodonovan3@gmail.com>
2024-06-19 12:21:42 +0100

    lintfix

--- src/components/Organisms/Carousel/Carousel.js
+++ src/components/Organisms/Carousel/Carousel.js
@@ -1,7 +1,7 @@
-const Carousel = () => {
-  return (
-    <h1>Generic Carousel Component</h1>
-  )
-}
+import React from 'react';
 
-export default Carousel
+const Carousel = () => (
+  <h1>Generic Carousel Component</h1>
+);
+
+export default Carousel;
