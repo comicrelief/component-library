fd8ace1babfd38936cb709a67aa6a30146217eda
AndyEPhipps <a.phipps@comicrelief.com>
2024-11-01 13:21:53 +0000

    Remove unneccessary import

--- src/components/Atoms/Button/Button.js
+++ src/components/Atoms/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import spacing from '../../../theme/shared/spacing';
 
 const StyledButton = styled.button`
