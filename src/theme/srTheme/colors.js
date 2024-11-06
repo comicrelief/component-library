8a10cbbb2d5e99bdc6a916ded4ff96c50de4e807
Jon Mulhern <81927768+curlyfriesplease@users.noreply.github.com>
2024-11-06 11:23:24 +0000

    feat: adds blue_donate_interact colour

--- src/theme/srTheme/colors.js
+++ src/theme/srTheme/colors.js
@@ -4,6 +4,7 @@ const colors = {
   blue: '#223c59',
   blue_dark: '#3a4972',
   blue_donate: '#2042AD',
+  blue_donate_interact: '#001A85',
   blue_light: '#274084',
   blue_royal: '#6489FC',
   coral: '#FF6F61',
