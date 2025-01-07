a31de968e9747b01d28c83144e6ba96d9b15bdac
AndyEPhipps <a.phipps@comicrelief.com>
2025-01-07 09:11:54 +0000

    Snap update

--- src/components/Atoms/Logo/Logo.test.js
+++ src/components/Atoms/Logo/Logo.test.js
@@ -25,7 +25,7 @@ it("renders correctly", () => {
       vertical-align: bottom;
     }
 
-    @media (min-width:1175px) {
+    @media (min-width:1300px) {
       .c0 {
         width: 70px;
       }
