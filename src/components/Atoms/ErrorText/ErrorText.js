7b8833f51a599e4a411326862309c5e047d4e75e
Micheal O'Donovan <mikeodonovan3@gmail.com>
2025-03-03 17:21:05 +0000

    clean out ErrorText

--- src/components/Atoms/ErrorText/ErrorText.js
+++ src/components/Atoms/ErrorText/ErrorText.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Text from '../Text/Text';
-import spacing from '../../../theme/shared/spacing';
 
 const ErrorIcon = styled.span`
   display: inline-block;
-  padding: ${spacing('m')} ${spacing('md')} ${spacing('md')};
   width: 100%;
   z-index: 1;
-  margin-top: -10px;
-  border-radius: ${spacing('sm')};
   font-weight: bold;
+  padding: 0.6rem 1rem 1rem;
 `;
 
 const ErrorText = ({ children, ...rest }) => (
