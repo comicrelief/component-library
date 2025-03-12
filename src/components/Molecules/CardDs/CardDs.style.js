797eace48f04ce8e716e1aeec4d31e40acf914fa
Micheal O'Donovan <mikeodonovan3@gmail.com>
2025-03-12 11:39:10 +0000

    adjust button position basics

--- src/components/Molecules/CardDs/CardDs.style.js
+++ src/components/Molecules/CardDs/CardDs.style.js
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components';
-import spacing from '../../../theme/shared/spacing';
 import zIndex from '../../../theme/shared/zIndex';
 
 const Container = styled.div`
@@ -23,17 +22,17 @@ const Container = styled.div`
 
 const Image = styled.div`
   height: auto;
-  margin: 0 0 0 ${spacing('m')};
+  margin: 0 0 0 1.5rem;
 
   ${({ isCarousel, theme }) => !isCarousel && css`
     @media ${theme.allBreakpoints('M')} {
-      margin: 0 -${spacing('m')} 0 ${spacing('m')};
+      margin: 0 -1.5rem 0 1.5rem;
     }
   `};
 
   img {
-    border-radius: ${spacing('md')};
-    box-shadow: 0 0 ${spacing('md')} rgba(0, 0, 0, 0.15);
+    border-radius: 1rem;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.15);
   }
 `;
 
@@ -52,14 +51,14 @@ const MediaLink = styled.a`
 `;
 
 const Copy = styled.div`
-  padding: ${spacing('l')};
-  ${({ hasLink }) => hasLink && `padding-bottom: ${spacing('xl')}`};
+  padding: 2rem;
+  ${({ hasLink }) => hasLink && `padding-bottom: 4rem`};
   display: flex;
   flex-direction: column;
-  border-radius: ${spacing('md')};
-  box-shadow: 0 0 ${spacing('md')} rgba(0, 0, 0, 0.15);
+  border-radius: 1rem;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.15);
   background: ${({ theme, backgroundColor }) => theme.color(backgroundColor)};
-  margin: 0 ${spacing('m')} 0 0;
+  margin: 0 1.5rem 0 0;
 
   @media ${({ theme }) => theme.allBreakpoints('XL')} {
     height: 100%;
@@ -68,18 +67,18 @@ const Copy = styled.div`
   ${zIndex('low')};
 
   ${({ hasImage, isCarousel, theme }) => hasImage && css`
-    margin-top: calc(-2 * ${spacing('m')});
-    min-height: calc(5 * ${spacing('l')});
+    margin-top: calc(-2 * 1.5rem});
+    min-height: calc(5 * 2rem);
 
     ${!isCarousel && css`
       @media ${theme.allBreakpoints('M')} {
-        margin: ${spacing('m')} 0 -${spacing('m')} -${spacing('m')};
+        margin: 1.5rem 0 -1.5rem -1.5rem;
         width: calc(50% + 6rem);
       }
     `};
 
     @media ${theme.allBreakpoints('XL')} {
-      margin: calc(-2 * ${spacing('m')}) 0 -${spacing('m')} 0;
+      margin: calc(-2 * 1.5rem 0 -1.5rem 0);
       width: 100%;
     }
   `};
@@ -87,15 +86,15 @@ const Copy = styled.div`
 
 const CTA = styled.div`
   position: absolute;
-  right: ${spacing('m')};
-  bottom: -${spacing('m')};
+  right: 3rem;
+  bottom: -1.5rem;
 
   ${zIndex('medium')};
 
   ${({ isCarousel, theme }) => !isCarousel && css`
     @media ${theme.allBreakpoints('M')} {
-      bottom: calc(-1 * (${spacing('l')} + ${spacing('md')}));
-      ${({ hasImage }) => !hasImage && `bottom: -${spacing('m')};`};
+      bottom: calc(-1 * (2rem + 1rem));
+      ${({ hasImage }) => !hasImage && `bottom: -1.5rem;`};
     }
   `};
 `;
