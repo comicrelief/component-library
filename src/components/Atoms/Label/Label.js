5dcfb04007d4ff4343b11ba85f596403b36b63ff
Micheal O'Donovan <mikeodonovan3@gmail.com>
2025-03-17 17:03:13 +0000

    remove Label changes

--- src/components/Atoms/Label/Label.js
+++ src/components/Atoms/Label/Label.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import {
-  LabelElement,
-  VisibleText,
-  HiddenText
-} from './Label.style';
+import Text from '../Text/Text';
+import spacing from '../../../theme/shared/spacing';
+import hideVisually from '../../../theme/shared/hideVisually';
+
+const LabelElement = styled.label`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme, errorMsg }) => (errorMsg ? theme.color('red') : theme.color('grey_label'))};
+
+  ${({ optional, theme }) => optional === true && `
+  :after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    content: 'Optional';
+    font-family: ${theme.fontFamilies(theme.font.regular)};
+    font-size: ${theme.fontSize('s')};
+  }`}
+`;
+
+const VisibleText = styled(Text)`
+  margin-bottom: ${spacing('sm')};
+  font-weight: normal;
+`;
+
+const HiddenText = styled(Text)`${hideVisually}`;
 
 // eslint-disable-next-line react/prop-types
 const LabelText = ({
