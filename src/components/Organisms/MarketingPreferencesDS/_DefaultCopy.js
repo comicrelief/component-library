3bf44374e5c21fe760baecfd5cbf191ec83d788c
AndyEPhipps <a.phipps@comicrelief.com>
2025-09-29 10:52:52 +0100

    feat: MP style fixes

--- src/components/Organisms/MarketingPreferencesDS/_DefaultCopy.js
+++ src/components/Organisms/MarketingPreferencesDS/_DefaultCopy.js
@@ -4,21 +4,21 @@ import Link from '../../Atoms/Link/Link';
 
 const defaultCopyTop = (
   <div>
-    <Text tag="h3" size="xl" family="Anton" uppercase weight="400" color="grey_dark">
+    <Text tag="h3" size="m" family="Montserrat" weight="600" color="black">
       Stay up to date with Comic Relief?
     </Text>
-    <Text tag="p" size="md" color="grey_dark">
+    <Text tag="p" size="md" color="black">
       We’d like to send you updates on the work we’re funding and how
       your support makes a difference in the UK and around the world.
     </Text>
-    <Text tag="p" size="md" color="grey_dark">
+    <Text tag="p" size="md" color="black">
       Please tick the relevant boxes below if you would like to hear from us.
     </Text>
   </div>
 );
 
 const defaultCopyBottom = (
-  <Text tag="p" size="xs" color="grey_dark">
+  <Text tag="p" size="xs" color="black">
     Update your preferences at any time by visiting our
     {' '}
     <Link
