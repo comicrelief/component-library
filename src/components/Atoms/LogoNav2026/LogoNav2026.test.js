27b4a3a39b63cce154d8a72b0f9ddf52c487329d
Micheal O'Donovan <mikeodonovan3@gmail.com>
2025-12-01 11:05:48 +0000

    snaps

--- src/components/Atoms/LogoNav2026/LogoNav2026.test.js
+++ src/components/Atoms/LogoNav2026/LogoNav2026.test.js
@@ -7,6 +7,11 @@ it('renders correctly', () => {
   const tree = renderWithTheme(<LogoNav2026 rotate />).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
+    .c0 {
+      z-index: 3;
+      width: 50px;
+    }
+
     .c1 {
       display: block;
       width: auto;
@@ -14,14 +19,29 @@ it('renders correctly', () => {
       max-width: 100%;
     }
 
-    .c0 {
-      z-index: 3;
-      width: 150px;
+    .c2 {
+      display: block;
     }
 
-    @media (min-width:1150px) {
+    .c3 {
+      display: none;
+    }
+
+    @media (min-width:740px) {
       .c0 {
-        width: 70px;
+        width: 180px;
+      }
+    }
+
+    @media (min-width:320px) {
+      .c2 {
+        display: none;
+      }
+    }
+
+    @media (min-width:320px) {
+      .c3 {
+        display: block;
       }
     }
 
@@ -31,8 +51,14 @@ it('renders correctly', () => {
     >
       <img
         alt="Comic Relief logo"
-        className="c1"
-        data-testid="Image"
+        className="c1 c2"
+        data-testid="MobileImage"
+        src="mock.asset"
+      />
+      <img
+        alt="Comic Relief logo"
+        className="c1 c3"
+        data-testid="DesktopImage"
         src="mock.asset"
       />
     </div>
