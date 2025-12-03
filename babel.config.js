e445b1c5859098b52d7e4e6a367123444a387f24
AndyEPhipps <a.phipps@comicrelief.com>
2025-12-03 14:52:24 +0000

    Try rolling back just a few things

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,13 @@
 // eslint-disable-next-line func-names
 module.exports = function (api) {
-    api.cache(true);
+  api.cache(true);
 
-    const presets = [ ["react-app", { "absoluteRuntime": false }] ];
-    const plugins = [
-        ["babel-plugin-styled-components"],
-        ["import", { "libraryName": "lodash", "libraryDirectory": "", "camel2DashComponentName": false }]
-    ];
+  const presets = [['react-app', { absoluteRuntime: false }]];
+  const plugins = [
+    ['babel-plugin-styled-components']];
 
-    return {
-        presets,
-        plugins
-    };
-}
+  return {
+    presets,
+    plugins
+  };
+};
