7a58bc24caba2ddfb0aac09883ce0a1f4d4d08c4
Micheal O'Donovan <76488110+michael-odonovan@users.noreply.github.com>
2025-12-05 12:10:36 +0000

    feat: adjust breakpoints (#779)
    * fix
    
    * Empty commit to trigger build
    
    * Empty commit to trigger build
    
    * merge main/master

--- src/theme/shared/breakpoints2026.js
+++ src/theme/shared/breakpoints2026.js
@@ -1,10 +1,19 @@
+/**
+ * Example Usage
+
+Make height: 200px from 1024px onwards:
+
+@media ${({ theme }) => theme.breakpoints2026('L')} {
+  height: 200px;
+}
+
+*/
+
 export const breakpointValues2026 = {
   XS: 0,
   S: 320,
   M: 740,
   L: 1024,
-  Nav: 1150,
-  NavWide: 1350,
   XL: 1440
 };
 
