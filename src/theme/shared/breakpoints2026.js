90b6dc68d3b8d6a6049c117dde4dd9d568c306b6
AndyEPhipps <a.phipps@comicrelief.com>
2025-12-05 11:08:36 +0000

    feat: adding new WebsiteProject breakpoints

---
+++ src/theme/shared/breakpoints2026.js
@@ -0,0 +1,16 @@
+export const breakpointValues2026 = {
+  XS: 0,
+  S: 320,
+  M: 740,
+  L: 1024,
+  Nav: 1150,
+  NavWide: 1350,
+  XL: 1440
+};
+
+export default size => {
+  if (size) {
+    return `(min-width: ${breakpointValues2026[size]}px)`;
+  }
+  return 'inherit';
+};
