45204a488f7e06f560227d74dc08bc31f3cde8e3
Jon Mulhern <81927768+curlyfriesplease@users.noreply.github.com>
2025-12-08 16:04:29 +0000

    For footer examples, import data from its own file rather than list out within the example .md file.

---
+++ src/components/Organisms/Footer/dev-data/data.js
@@ -0,0 +1,89 @@
+export const primaryLinksList = [
+  {
+    title: 'Contact us',
+    path: 'contact-us',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Update your preferences',
+    path: 'https://www.comicrelief.com/update-your-preferences',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Your Gift Aid',
+    path: 'https://giftaid.comicrelief.com/update',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Reporting complaints and concerns',
+    path: 'https://www.comicrelief.com/reporting-complaints',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'FAQs',
+    path: 'https://www.comicrelief.com/frequently-asked-questions',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  }
+];
+
+export const secondaryLinksList = [
+  {
+    title: 'Terms of use',
+    path: 'https://www.comicrelief.com/terms-of-use',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Privacy notice',
+    path: 'https://www.comicrelief.com/privacy-notice',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Cookies',
+    path: 'https://www.comicrelief.com/cookies',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Text to donate terms',
+    path: 'https://www.comicrelief.com/text-to-donate-terms',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Prize draw terms',
+    path: 'https://www.comicrelief.com/prize-draw-terms',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Modern Slavery and Human Trafficking Statement',
+    path: 'https://www.comicrelief.com/modern-slavery-statement',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  },
+  {
+    title: 'Positive Practices',
+    path: 'https://www.comicrelief.com/positive-practices',
+    internal: {
+      type: 'ContentfulPageLandingPage'
+    }
+  }
+];
