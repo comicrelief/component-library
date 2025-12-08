6f6bc1055a984ed03952bcde8e4800dc34bd1b50
AndyEPhipps <a.phipps@comicrelief.com>
2025-12-08 15:28:24 +0000

    Restore old fontSizes

--- src/theme/crTheme/fontSizes.js
+++ src/theme/crTheme/fontSizes.js
@@ -1,13 +1,16 @@
 const fontSizes = {
-  xxs: '0.875rem',
-  xs: '1rem',
-  s: '1.125rem',
+  xxs: '0.6875rem',
+  xs: '0.75rem',
+  s: '1rem',
   m: '1.25rem',
   l: '1.5rem',
-  xl: '1.875rem',
-  xxl: '2rem',
-  big: '2.5rem',
-  super: '3rem'
+  xl: '2rem',
+  xxl: '3rem',
+  big: '4rem',
+  super: '5rem',
+  // Temporary edgecase fix to at least keep all sizing centralise
+  // before website project design review overhaul
+  error: '0.875rem'
 };
 
 export default size => {
