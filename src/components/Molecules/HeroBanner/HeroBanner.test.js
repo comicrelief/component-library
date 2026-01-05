85b1a2937862f13936a28aa9dbd177c598c4a226
AndyEPhipps <a.phipps@comicrelief.com>
2026-01-05 12:41:39 +0000

    Attempt to fix test

--- src/components/Molecules/HeroBanner/HeroBanner.test.js
+++ src/components/Molecules/HeroBanner/HeroBanner.test.js
@@ -4,71 +4,32 @@ import renderWithTheme from '../../../../tests/hoc/shallowWithTheme';
 import HeroBanner from './HeroBanner';
 import Text from '../../Atoms/Text/Text';
 import Link from '../../Atoms/Link/Link';
+import variants from './_variants';
 
 const defaultData = require('../../../styleguide/data/data').defaultData;
-it('renders Promo correctly', () => {
+
+it('renders HeroBanner correctly', () => {
   const tree = renderWithTheme(
     <HeroBanner
-      backgroundColor="blue_dark"
-      imageSet={defaultData.promoImage}
-      image={defaultData.promoImage}
-      imageAltText=""
-      copyLeft={false}
+      pageBackgroundColour="teal_light"
+      imageSet={defaultData.headerBannerLG}
+      image={defaultData.headerBannerLG}
+      imageLow={defaultData.headerBannerLG}
+      imageAltText="Image alt text"
+      variant={variants.FULL_HEIGHT}
+      copyLeft
+      ctaLabel="Test CTA Label"
+      ctaLink="http://www.google.com"
     >
-      <Text
-        tag="h1"
-        color="white"
-        size="super"
-        family="Anton"
-        uppercase
-        weight="normal"
-      >
+    <Text
+      tag="h1"
+      color="black"
+    >
         Curabitur pretium tincidunt lacus
       </Text>
-      <Text tag="p" color="white" size="s">
-        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius,
-        turpis et commodo, est eros bibendum elit, nec luctus magna sollicitudin.
-      </Text>
-      <span>
-        <Link href="/" type="button" color="white">
-          Show me this
-        </Link>
-      </span>
-    </HeroBanner>
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-it('renders Promo correctly end position', () => {
-  const tree = renderWithTheme(
-    <HeroBanner
-      backgroundColor="blue_dark"
-      imageSet={defaultData.promoImage}
-      image={defaultData.promoImage}
-      imageAltText=""
-      copyLeft={false}
-      position="end"
-    >
-      <Text
-        tag="h1"
-        color="white"
-        size="super"
-        family="Anton"
-        uppercase
-        weight="normal"
-      >
-        Curabitur pretium tincidunt lacus
-      </Text>
-      <Text tag="p" color="white" size="s">
-        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius,
-        turpis et commodo, est eros bibendum elit, nec luctus magna sollicitudin.
-      </Text>
-      <span>
-        <Link href="/" type="button" color="white">
-          Show me this
-        </Link>
-      </span>
+    <Text tag="p" color="black">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </Text>
     </HeroBanner>
   ).toJSON();
 
