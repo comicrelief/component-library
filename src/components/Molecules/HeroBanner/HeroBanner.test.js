312fd8ab9f2f485e2d71fadc4666fd7692120430
AndyEPhipps <a.phipps@comicrelief.com>
2026-01-08 09:33:59 +0000

    Extend tests for all 3 variants

--- src/components/Molecules/HeroBanner/HeroBanner.test.js
+++ src/components/Molecules/HeroBanner/HeroBanner.test.js
@@ -8,7 +8,7 @@ import variants from './_variants';
 
 const defaultData = require('../../../styleguide/data/data').defaultData;
 
-it('renders HeroBanner correctly', () => {
+it('renders "Full Height Media" Hero Banner correctly', () => {
   const tree = renderWithTheme(
     <HeroBanner
       pageBackgroundColour="teal_light"
@@ -17,9 +17,61 @@ it('renders HeroBanner correctly', () => {
       imageLow={defaultData.headerBannerLG}
       imageAltText="Image alt text"
       variant={variants.FULL_HEIGHT}
-      copyLeft
       ctaText="Test CTA Label"
       ctaLink="http://www.google.com"
+      copyLeft
+    >
+    <Text
+      tag="h1"
+      color="black"
+    >
+        Curabitur pretium tincidunt lacus
+      </Text>
+    <Text tag="p" color="black">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </Text>
+    </HeroBanner>
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders "Half Height Media" Hero Banner correctly', () => {
+  const tree = renderWithTheme(
+    <HeroBanner
+      pageBackgroundColour="teal_light"
+      imageSet={defaultData.headerBannerLG}
+      image={defaultData.headerBannerLG}
+      imageLow={defaultData.headerBannerLG}
+      imageAltText="Image alt text"
+      variant={variants.HALF_HEIGHT}
+      ctaText="Test CTA Label"
+      ctaLink="http://www.google.com"
+      copyLeft={false}
+    >
+    <Text
+      tag="h1"
+      color="black"
+    >
+        Curabitur pretium tincidunt lacus
+      </Text>
+    <Text tag="p" color="black">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </Text>
+    </HeroBanner>
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders "Text Banner" Hero Banner correctly', () => {
+  const tree = renderWithTheme(
+    <HeroBanner
+      pageBackgroundColour="teal_light"
+      variant={variants.TEXT_BANNER}
+      ctaText="Test CTA Label"
+      ctaLink="http://www.google.com"
+      copyLeft={false}
     >
     <Text
       tag="h1"
