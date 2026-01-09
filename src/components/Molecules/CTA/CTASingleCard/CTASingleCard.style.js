1bb938f983f6570f42f7d5b6219577d6377653b5
Jon Mulhern <81927768+curlyfriesplease@users.noreply.github.com>
2026-01-09 16:30:38 +0000

    Slightly zoom image to avoid bounce issues

--- src/components/Molecules/CTA/CTASingleCard/CTASingleCard.style.js
+++ src/components/Molecules/CTA/CTASingleCard/CTASingleCard.style.js
@@ -9,6 +9,12 @@ const SingleCardContainer = styled.div`
   background: ${({ theme, backgroundColor }) => (
     backgroundColor ? theme.color(snakeCase(backgroundColor)) : 'transparent'
   )};
+
+  img {
+    // Zoom the image in slightly by default so the 'bounce' animation doesn't cause issues
+    transform: scale(1.02);
+    transition: transform ${0.4}s cubic-bezier(0.68, ${-1.15}, 0.265, ${2.35});
+   }
 `;
 
 export default SingleCardContainer;
