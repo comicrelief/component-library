787e4c8e7014b1af99d586c6a42d320e7ef85db7
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-01-20 17:09:40 +0000

    lintfix remove console.log

--- src/components/Organisms/Header2026/utils/urlHelper.js
+++ src/components/Organisms/Header2026/utils/urlHelper.js
@@ -2,7 +2,6 @@ const BASE_URL = 'https://www.comicrelief.com';
 
 // Helper to prepend base URL to paths when in dev mode
 const prependBaseUrl = (path, devMode) => {
-  console.log('prependBaseUrl called:', { path, devMode });
   if (!devMode || !path) return path;
   // Don't prepend if it's already an absolute URL
   if (path.startsWith('http://') || path.startsWith('https://')) return path;
