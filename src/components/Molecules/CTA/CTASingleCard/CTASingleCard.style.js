535ce852bc41bafc27328c64955329e2e40283c6
Jon Mulhern <81927768+curlyfriesplease@users.noreply.github.com>
2026-01-28 13:04:39 +0000

    Adds missing padding to CTASingleCard

--- src/components/Molecules/CTA/CTASingleCard/CTASingleCard.style.js
+++ src/components/Molecules/CTA/CTASingleCard/CTASingleCard.style.js
@@ -3,6 +3,13 @@ import styled from 'styled-components';
 export const SingleCardSection = styled.div`
   padding-top: ${({ paddingAbove }) => paddingAbove};
   padding-bottom: ${({ paddingBelow }) => paddingBelow};
+  padding-inline: 1rem;
+  @media ${({ theme }) => theme.breakpoints2026('M')} {
+    padding-inline: 2rem;
+  }
+  @media ${({ theme }) => theme.breakpoints2026('L')} {
+    padding-inline: 4rem;
+  }
   width: 100%;
   background: ${({ theme, backgroundColor }) => theme.color(backgroundColor)};
 `;
