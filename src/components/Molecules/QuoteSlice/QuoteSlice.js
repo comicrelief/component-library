d8d934d93729f13d4e7a0177d8d2546f42a29c1f
Tom Evans <tom@acatcalledfrank.com>
2026-02-25 10:15:57 +0000

    feat: adding responsive font sizes for quote text (#842)
    * shrink quote font size based on text length
    
    * feat: adding responsive font sizes for quote text
    
    * moving ratio var into styled component
    
    * clarifying comments and fixing typo
    
    * fix: updating test snapshot
    
    * implementing design feedback
    
    - fixing shadows
    - moving height limits onto inner wrapper
    - upping desktop font size and lowering character floor
    
    * fix: restoring empty card bg
    
    * Update QuoteSlice.test.js.snap
    
    ---------
    
    Co-authored-by: AndyEPhipps <a.phipps@comicrelief.com>

--- src/components/Molecules/QuoteSlice/QuoteSlice.js
+++ src/components/Molecules/QuoteSlice/QuoteSlice.js
@@ -4,13 +4,13 @@ import MediaQuery from 'react-responsive';
 import { breakpointValues2026 as breakpointValues } from '../../../theme/shared/breakpoints2026';
 import Picture from '../../Atoms/Picture/Picture';
 import {
+  Body,
   CTAButton,
   Heading,
   InnerWrapper,
   OuterWrapper,
   PictureWrapper,
-  TextWrapper,
-  Body
+  TextWrapper
 } from './QuoteSlice.style';
 
 const QuoteSlice = ({
@@ -31,6 +31,35 @@ const QuoteSlice = ({
 }) => {
   const hasLink = Boolean(ctaUrl && ctaLabel);
 
+  // scale heading (the main quote) text as the text grows longer:
+  // - shorter quotes, up to 100 characters, will be full size
+  // - longer quotes, over 500 characters, will be at the smallest size
+  // - mid-length quotes, between 100 and 500 characters, will scale down proportionally
+  const minCharacterCount = 30;
+  const maxCharacterCount = 500;
+  const characterCount = heading?.length;
+
+  // we calculate the ratio and set it as a CSS variable;
+  // the heading text uses a CSS clamp function to enfore a min and max font size
+  // so we don't go too small or too large;
+  // this also lets us use CSS breakpoints to set different min/max sizes for different screen sizes
+  let ratio = 1;
+
+  switch (true) {
+    // small quotes
+    case characterCount < minCharacterCount:
+      ratio = 1;
+      break;
+    // longer quotes
+    case characterCount > maxCharacterCount:
+      ratio = 0.3;
+      break;
+    // mid-length quotes
+    default:
+      ratio = 1 - (characterCount - minCharacterCount) / (maxCharacterCount - minCharacterCount);
+      break;
+  }
+
   return (
     <OuterWrapper
       backgroundColour={pageBackgroundColour}
@@ -41,23 +70,47 @@ const QuoteSlice = ({
       <InnerWrapper>
         <PictureWrapper>
           <MediaQuery maxWidth={breakpointValues.M - 1}>
-            <Picture height="100%" objectFit="cover" images={imageS} imageLow={imageSPlaceholder} />
+            <Picture
+              height="100%"
+              objectFit="cover"
+              images={imageS}
+              imageLow={imageSPlaceholder}
+            />
           </MediaQuery>
           <MediaQuery
             minWidth={breakpointValues.M}
             maxWidth={breakpointValues.L - 1}
           >
-            <Picture height="100%" objectFit="cover" images={imageM} imageLow={imageMPlaceholder} />
+            <Picture
+              height="100%"
+              objectFit="cover"
+              images={imageM}
+              imageLow={imageMPlaceholder}
+            />
           </MediaQuery>
           <MediaQuery minWidth={breakpointValues.L}>
-            <Picture height="100%" objectFit="cover" images={imageL} imageLow={imageLPlaceholder} />
+            <Picture
+              height="100%"
+              objectFit="cover"
+              images={imageL}
+              imageLow={imageLPlaceholder}
+            />
           </MediaQuery>
         </PictureWrapper>
         <TextWrapper>
-          {heading && <Heading tag="h2">{heading}</Heading>}
+          {heading && (
+            <Heading tag="h2" ratio={ratio}>
+              {heading}
+            </Heading>
+          )}
           {body && <Body textAlign="center">{body}</Body>}
           {hasLink && (
-            <CTAButton type="button" href={ctaUrl} color={textColour} fullWidth={false}>
+            <CTAButton
+              type="button"
+              href={ctaUrl}
+              color={textColour}
+              fullWidth={false}
+            >
               {ctaLabel}
             </CTAButton>
           )}
