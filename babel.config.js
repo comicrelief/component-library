889ae33f2da8f3f27efb450ce24a22285438933c
Jon Mulhern <81927768+curlyfriesplease@users.noreply.github.com>
2026-04-28 12:53:02 +0100

    Cater to both CJS and ESM

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,34 @@
 // eslint-disable-next-line func-names
 module.exports = function (api) {
-  api.cache(true);
+  const format = process.env.MODULE_FORMAT;
 
-  const presets = [['react-app', { absoluteRuntime: false }]];
+  api.cache.using(() => `${format || 'node'}:${process.env.NODE_ENV || 'development'}`);
+
   const plugins = [
     ['babel-plugin-styled-components'],
     ['import', { libraryName: 'lodash', libraryDirectory: '', camel2DashComponentName: false }]
+  ];
+
+  const presetEnvOptions = { bugfixes: true };
+
+  if (format === 'esm') {
+    presetEnvOptions.modules = false;
+  } else {
+    presetEnvOptions.modules = 'commonjs';
+    if (!format) {
+      presetEnvOptions.targets = { node: 'current' };
+    }
+  }
+
+  const presets = [
+    ['@babel/preset-env', presetEnvOptions],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'classic',
+        development: process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
+      }
+    ]
   ];
 
   return {
