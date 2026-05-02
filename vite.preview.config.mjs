7ca323ba0c199f92f63a94360c54c76bf1fabfef
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-05-02 12:19:14 +0100

    vite build fix

--- vite.preview.config.mjs
+++ vite.preview.config.mjs
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // App build config (for Netlify preview) — no library mode, produces HTML output
 export default {
+  build: {
+    outDir: 'dist-preview',
+  },
   plugins: [
     {
       name: 'treat-js-as-jsx',
