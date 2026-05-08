066461da5a8834a30180999856c59473a044b31a
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-05-08 11:43:34 +0100

    snaps

--- src/components/Atoms/Link/Link.test.js
+++ src/components/Atoms/Link/Link.test.js
@@ -12,35 +12,35 @@ it('renders a standard styled link correctly', () => {
   ).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
-    .c0 {
-      position: relative;
-      display: inline;
-      color: #000000;
-      font-weight: normal;
-    }
+.c0 {
+  position: relative;
+  display: inline;
+  color: #000000;
+  font-weight: normal;
+  color: #E52630;
+}
 
-    .c0 :hover,
-    .c0 :focus {
-      color: #000000;
-      text-decoration: none;
-    }
+.c0:hover,
+.c0:focus {
+  color: #000000;
+  text-decoration: none;
+}
 
-    <a
-      buttonType="primary"
-      className="c0"
-      color="red"
-      fullWidth={false}
-      home={false}
-      href="/test"
-      iconFirst={false}
-      mobileColour={null}
-      target="_self"
-      type="standard"
-      underline={true}
-    >
-      A standard link
-    </a>
-  `);
+<a
+  buttonType="primary"
+  className="c0"
+  fullWidth={false}
+  home={false}
+  href="/test"
+  iconFirst={false}
+  mobileColour={null}
+  target="_self"
+  type="standard"
+  underline={true}
+>
+  A standard link
+</a>
+`);
 });
 
 it('renders a button with an icon', () => {
@@ -56,87 +56,87 @@ it('renders a button with an icon', () => {
   ).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
-    .c1 {
-      display: inline-flex;
-      margin-left: 1rem;
-      align-self: center;
-      right: 1.5rem;
-      position: absolute;
-      right: -2rem;
-      top: 0;
-      bottom: 0;
-    }
+.c1 {
+  display: inline-flex;
+  margin-left: 1rem;
+  align-self: center;
+  right: 1.5rem;
+  position: absolute;
+  right: -2rem;
+  top: 0;
+  bottom: 0;
+}
 
-    .c0 {
-      position: relative;
-      display: inline;
-      color: #000000;
-      font-weight: normal;
-    }
+.c0 {
+  position: relative;
+  display: inline;
+  color: #000000;
+  font-weight: normal;
+  color: #E52630;
+}
 
-    .c0 :hover,
-    .c0 :focus {
-      color: #000000;
-      text-decoration: none;
-    }
+.c0:hover,
+.c0:focus {
+  color: #000000;
+  text-decoration: none;
+}
 
-    .c2 {
-      fill: #FFFFFF;
-    }
+.c2 {
+  fill: #FFFFFF;
+}
 
-    @media (min-width: 740px) {
-      .c1 {
-        width: auto;
-        right: auto;
-        position: relative;
-        position: absolute;
-        right: -2rem;
-        top: 0;
-        bottom: 0;
-      }
-    }
+@media (min-width: 740px) {
+  .c1 {
+    width: auto;
+    right: auto;
+    position: relative;
+    position: absolute;
+    right: -2rem;
+    top: 0;
+    bottom: 0;
+  }
+}
 
-    @media (min-width: 1024px) {
-      .c2 {
-        fill: #FFFFFF;
-      }
-    }
+@media (min-width: 1024px) {
+  .c2 {
+    fill: #FFFFFF;
+  }
+}
 
-    <a
-      buttonType="primary"
-      className="c0"
-      color="red"
-      fullWidth={false}
-      home={false}
-      href="/test"
-      iconFirst={false}
+<a
+  buttonType="primary"
+  className="c0"
+  fullWidth={false}
+  home={false}
+  href="/test"
+  iconFirst={false}
+  mobileColour={null}
+  target="_self"
+  type="standard"
+  underline={true}
+>
+  Internal link
+  <span
+    className="c1"
+    type="standard"
+  >
+    <svg
+      className="c2"
+      colour="white"
+      fill="#FFFFFF"
+      height={24}
       mobileColour={null}
-      target="_self"
-      type="standard"
-      underline={true}
+      viewBox="0 0 96 96"
+      width={24}
+      xmlns="http://www.w3.org/2000/svg"
     >
-      Internal link
-      <span
-        className="c1"
-        type="standard"
-      >
-        <svg
-          className="c2"
-          colour="white"
-          fill="#FFFFFF"
-          height={24}
-          mobileColour={null}
-          viewBox="0 0 96 96"
-          width={24}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M90.871 50.776c-.016.016-.026.036-.043.052l-32 32C58.047 83.609 57.024 84 56 84s-2.047-.391-2.829-1.171a4 4 0 010-5.657L78.343 52H8a4 4 0 010-8h70.343L53.171 18.829a4 4 0 015.657-5.657l32 32c.016.016.027.036.043.052.167.172.321.357.455.557.069.104.118.216.176.325.065.121.138.236.191.364.057.137.09.279.13.42.032.109.074.214.097.327a4.017 4.017 0 010 1.568c-.023.113-.065.217-.097.327-.041.141-.074.283-.13.42-.053.128-.126.243-.191.364-.059.109-.107.221-.176.325a4.035 4.035 0 01-.455.555z"
-          />
-        </svg>
-      </span>
-    </a>
-  `);
+      <path
+        d="M90.871 50.776c-.016.016-.026.036-.043.052l-32 32C58.047 83.609 57.024 84 56 84s-2.047-.391-2.829-1.171a4 4 0 010-5.657L78.343 52H8a4 4 0 010-8h70.343L53.171 18.829a4 4 0 015.657-5.657l32 32c.016.016.027.036.043.052.167.172.321.357.455.557.069.104.118.216.176.325.065.121.138.236.191.364.057.137.09.279.13.42.032.109.074.214.097.327a4.017 4.017 0 010 1.568c-.023.113-.065.217-.097.327-.041.141-.074.283-.13.42-.053.128-.126.243-.191.364-.059.109-.107.221-.176.325a4.035 4.035 0 01-.455.555z"
+      />
+    </svg>
+  </span>
+</a>
+`);
 });
 
 it('renders a standard styled link correctly with target blank and assistive text', () => {
@@ -147,53 +147,53 @@ it('renders a standard styled link correctly with target blank and assistive tex
   ).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
-    .c0 {
-      position: relative;
-      display: inline;
-      color: #000000;
-      font-weight: normal;
-    }
+.c0 {
+  position: relative;
+  display: inline;
+  color: #000000;
+  font-weight: normal;
+  color: #E52630;
+}
 
-    .c0 :hover,
-    .c0 :focus {
-      color: #000000;
-      text-decoration: none;
-    }
+.c0:hover,
+.c0:focus {
+  color: #000000;
+  text-decoration: none;
+}
 
-    .c1 {
-      border: 0;
-      clip: rect(0 0 0 0);
-      clip-path: inset(50%);
-      height: 1px;
-      margin: -1px;
-      overflow: hidden;
-      padding: 0;
-      position: absolute;
-      white-space: nowrap;
-      width: 1px;
-    }
+.c1 {
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
 
-    <a
-      buttonType="primary"
-      className="c0"
-      color="red"
-      fullWidth={false}
-      home={false}
-      href="/test"
-      iconFirst={false}
-      mobileColour={null}
-      target="_blank"
-      type="standard"
-      underline={true}
-    >
-      A standard link
-      <span
-        className="c1"
-      >
-        (opens in new window)
-      </span>
-    </a>
-  `);
+<a
+  buttonType="primary"
+  className="c0"
+  fullWidth={false}
+  home={false}
+  href="/test"
+  iconFirst={false}
+  mobileColour={null}
+  target="_blank"
+  type="standard"
+  underline={true}
+>
+  A standard link
+  <span
+    className="c1"
+  >
+    (opens in new window)
+  </span>
+</a>
+`);
 });
 
 it('renders link styled as button correctly', () => {
@@ -204,82 +204,81 @@ it('renders link styled as button correctly', () => {
   ).toJSON();
 
   expect(tree).toMatchInlineSnapshot(`
-    .c0 {
-      display: inline-flex;
-      position: relative;
-      text-decoration: none;
-      transition: all 0.2s;
-      height: 2.5rem;
-      width: 100%;
-      justify-content: center;
-      align-items: center;
-      border: none;
-      cursor: pointer;
-      padding: 0.6rem 1rem;
-      border-radius: 0.5rem;
-      background-color: #FFE400;
-      color: #000000;
-      font-family: 'Montserrat',Helvetica,Arial,sans-serif;
-      font-weight: 700;
-      text-transform: inherit;
-      letter-spacing: 0;
-      font-size: 1rem;
-      line-height: 1.25rem;
-    }
+.c0 {
+  display: inline-flex;
+  position: relative;
+  text-decoration: none;
+  transition: all 0.2s;
+  height: 2.5rem;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  padding: 0.6rem 1rem;
+  border-radius: 0.5rem;
+  background-color: #FFE400;
+  color: #000000;
+  font-family: 'Montserrat',Helvetica,Arial,sans-serif;
+  font-weight: 700;
+  text-transform: inherit;
+  letter-spacing: 0;
+  font-size: 1rem;
+  line-height: 1.25rem;
+}
 
-    .c0:hover,
-    .c0:focus,
-    .c0:focus-within,
-    .c0:focus-visible {
-      background-color: #FEFD5A;
-      outline-offset: 3px;
-    }
+.c0:hover,
+.c0:focus,
+.c0:focus-within,
+.c0:focus-visible {
+  background-color: #FEFD5A;
+  outline-offset: 3px;
+}
 
-    .c0:disabled {
-      cursor: not-allowed;
-      opacity: 0.5;
-    }
+.c0:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
 
-    @media (min-width: 740px) {
-      .c0 {
-        font-size: 1rem;
-        line-height: 1.25rem;
-      }
-    }
+@media (min-width: 740px) {
+  .c0 {
+    font-size: 1rem;
+    line-height: 1.25rem;
+  }
+}
 
-    @media (min-width: 1024px) {
-      .c0 {
-        font-size: 1.125rem;
-        line-height: 1.375rem;
-      }
-    }
+@media (min-width: 1024px) {
+  .c0 {
+    font-size: 1.125rem;
+    line-height: 1.375rem;
+  }
+}
 
-    @media (min-width: 740px) {
-      .c0 {
-        width: auto;
-      }
-    }
+@media (min-width: 740px) {
+  .c0 {
+    width: auto;
+  }
+}
 
-    @media (min-width: 1024px) {
-      .c0 {
-        width: auto;
-      }
-    }
+@media (min-width: 1024px) {
+  .c0 {
+    width: auto;
+  }
+}
 
-    <a
-      buttonType="primary"
-      className="c0"
-      color="yellow"
-      fullWidth={false}
-      home={false}
-      href="/test"
-      iconFirst={false}
-      mobileColour={null}
-      target="_self"
-      type="button"
-      underline={true}
-    >
-      A yellow button
-    </a>
-  `);
+<a
+  buttonType="primary"
+  className="c0"
+  fullWidth={false}
+  home={false}
+  href="/test"
+  iconFirst={false}
+  mobileColour={null}
+  target="_self"
+  type="button"
+  underline={true}
+>
+  A yellow button
+</a>
+`);
 });
