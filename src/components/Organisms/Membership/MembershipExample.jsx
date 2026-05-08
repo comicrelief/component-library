baed1c2816edcc08cb3edc65db17d29d61009d9b
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-05-08 17:51:33 +0100

    fix Membership example

--- src/components/Organisms/Membership/MembershipExample.jsx
+++ src/components/Organisms/Membership/MembershipExample.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import Membership from './Membership';
 import data from './dev-data/data';
+import { defaultData } from '../../../data/data';
 
 export default function MembershipExample() {
   return (
-    <Membership
-      data={data}
-      mbshipID="mship-1"
-      cartID={data.cartId}
-      donateLink="https://donate.comicrelief.com/form/giftaid"
-      title="Support our work"
-      subtitle="Give a regular gift and help change lives."
-      otherDescription="Your donation will help us fund amazing projects in the UK and around the world."
-    />
+    <div>
+      <h3>Form align right</h3>
+      <Membership
+        alt="Background image"
+        backgroundColor="orange_light"
+        formAligntRight
+        imageLow={defaultData.imageLow}
+        images={defaultData.images}
+        data={data}
+        mbshipID="mship-1"
+        donateLink="https://donation.comicrelief.com/"
+        cartID="refugee1-365"
+        title="Help someone like Jordan see a better tomorrow"
+        subtitle="Jordan was close to suicide - which is now the biggest killer of men under 45 in the UK. Join now and help save lives."
+        otherDescription="Your donation will help us fund amazing projects in the UK and around the world."
+      />
+
+      <h3>Form align left</h3>
+      <Membership
+        backgroundColor="orange_light"
+        formAligntRight={false}
+        imageLow={defaultData.imageLow}
+        images={defaultData.images}
+        data={data}
+        mbshipID="mship-1"
+        donateLink="https://donation.comicrelief.com/"
+        cartID="refugee1-365"
+        title="Help someone like Jordan see a better tomorrow"
+        subtitle="Jordan was close to suicide - which is now the biggest killer of men under 45 in the UK. Join now and help save lives."
+        otherDescription="Your donation will help us fund amazing projects in the UK and around the world."
+      />
+    </div>
   );
 }
