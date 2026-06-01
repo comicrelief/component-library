53a1b97301803303a0e84222f27777073fe5f092
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-06-01 16:50:00 +0100

    ButtonWithStates xtra manual checks

--- src/components/Atoms/ButtonWithStates/ButtonWithStatesExample.jsx
+++ src/components/Atoms/ButtonWithStates/ButtonWithStatesExample.jsx
@@ -3,8 +3,23 @@ import ButtonWithStates from './ButtonWithStates';
 
 export default function ButtonWithStatesExample() {
   return (
-    <ButtonWithStates type="submit" loading disabled>
-      Enter prize draw
-    </ButtonWithStates>
+    <>
+
+      <ButtonWithStates
+        type="submit"
+        loading disabled
+      >
+        Enter prize draw
+      </ButtonWithStates>
+
+      <h3 style={{marginTop: "50px"}}>Children are an optional prop. Example to demonstrate the ButtonWithStates will work even with no children provided:</h3>
+
+      <ButtonWithStates
+        type="submit"
+        loading
+        loadingText="Submitting…"
+      />
+
+    </>
   );
 }
