c566bdd0fa918ed86722012b13df8f645c8a905e
Micheal O'Donovan <mikeodonovan3@gmail.com>
2026-08-28 09:32:36 +0100

    fix test path blah

--- src/components/Molecules/PostcodeLookup/PostcodeLookup.test.js
+++ src/components/Molecules/PostcodeLookup/PostcodeLookup.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'jest-styled-components';
 
-import renderWithTheme from '../../../hoc/shallowWithTheme';
+import renderWithTheme from '../../../../tests/hoc/shallowWithTheme';
 import PostcodeLookup from './PostcodeLookup';
 
 it('renders correctly', () => {
